e907419f199dfbe326d4c97b2eb8110581fbd7a7
Simon Speich <simon.speich@gmail.com>
2012-12-18 21:32:42 +0100

    fixed typo

--- photo/bildrechte.php
+++ photo/bildrechte.php
@@ -10,6 +10,6 @@
 <?php require_once 'inc_body_begin.php'; ?>
 <?php echo $_SERVER['DOCUMENT_ROOT']; ?>
 <h1>Bildrechte</h1>
-<p>Die abgebildeten Fotos können Auschnitte darstellen.</p>
+<p>Die abgebildeten Fotos können Ausschnitte darstellen.</p>
 </body>
 </html>
