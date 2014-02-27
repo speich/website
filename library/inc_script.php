ee6f25b05eb297570a18ab8a558ba052763c8c83
Simon Speich <simon.speich@gmail.com>
2014-02-27 13:07:33 +0100

    added Switzerland as keyword to meta tag

--- library/inc_script.php
+++ library/inc_script.php
@@ -25,12 +25,12 @@ ini_set('default_charset', $web->charset);
 if ($lang === 'de') {
 	$windowTitle = 'Fotografie und Webprogrammierung';
 	$metaDescription = 'Website von Simon Speich über Fotografie und Webprogrammierung';
-	$metaKeywords = 'Simon Speich, Fotografie, Webprogrammierung, Bilddatenbank, dojo, dojotoolkit, JavaScript, PHP, Foto, Photographie';
+	$metaKeywords = 'Simon Speich, Schweiz, Fotografie, Webprogrammierung, Bilddatenbank, dojo, dojotoolkit, JavaScript, PHP, Foto, Photographie';
 }
 else {
 	$windowTitle = 'Photography and web programming';
 	$metaDescription = 'Simon Speich\'s website about photography and web programming';
-	$metaKeywords = 'Simon Speich, photography, web programming, photo archive, dojo, dojotoolkit, JavaScript, PHP';
+	$metaKeywords = 'Simon Speich, Switzerland, photography, web programming, photo archive, dojo, dojotoolkit, JavaScript, PHP';
 }
 $web->pageTitle = 'speich.net - '.$windowTitle;
 
