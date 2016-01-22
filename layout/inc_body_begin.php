75712013ae0ad21f8068f2161670f0ea2c486d8b
Simon <simon.speich@gmail.com>
2016-01-22 14:54:25 +0100

    use index.php instead of default.php

--- layout/inc_body_begin.php
+++ layout/inc_body_begin.php
@@ -1,4 +1,4 @@
-<div id="layoutTop01"><a href="<?php echo $web->getWebRoot().'default.php'; ?>"><img src="<?php echo $web->getWebRoot().'layout/images/layout-logo.gif'; ?>" title="Simon Speich | speich.net" alt="speich.net logo"></a></div>
+<div id="layoutTop01"><a href="<?php echo $web->getWebRoot().'index.php'; ?>"><img src="<?php echo $web->getWebRoot().'layout/images/layout-logo.gif'; ?>" title="Simon Speich | speich.net" alt="speich.net logo"></a></div>
 <div id="layoutTop02"><?php echo $mainNav->render(); echo $web->renderLangNav(); ?></div>
 <div id="layoutMiddle">
 <div id="layoutNav"><?php echo $sideNav->render(); ?></div>
