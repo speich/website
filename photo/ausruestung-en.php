cf60656eeba7b803320463d44beeeed9cd4b1a57
Simon <simon.speich@gmail.com>
2017-05-06 13:29:49 +0200

    updated equipment information and made it more consistent

--- photo/ausruestung-en.php
+++ photo/ausruestung-en.php
@@ -14,48 +14,56 @@
 <h2>Cameras</h2>
 <ul class="main">
 <li>Nikon D810 (29.12.2014)</li>
-<li>Sony Cybershot RX100 (06.12.2013)</li>
-<li>Nikon D800 (21.03.2012)</li>
-<li class="formerEquipment">Nikon D300 (08.01.2009 - 18.08.2014)</li>
-<li class="formerEquipment">Nikon Coolpix P4 (19.05.2006 - 06.12.2013)</li>
+<li>Nikon D500 (15.12.2016)</li>
 <li>Nikon F100 (12.06.2004)</li>
-<li class="formerEquipment">Nikon F90x (1996 - 2004)</li>
-<li class="formerEquipment">Ricoh Singlex (- 2006)</li>
+<li>Sony Cybershot RX100 (06.12.2013)</li>
 <li>Minox 35 GL </li>
 </ul>
 <h2>Lenses</h2>
 <ul class="main">
-<li>600mm f/4 FL ED VR (12.08.2015)</li>
-<li>24mm f/1.4G ED AF-S NIKKOR (26.03.2014)</li>
-<li>80-400mm f/4.5-5.6G ED VR AF-S Nikkor (20.09.2013)</li>
-<li>18-200mm f/3.5-5.6 G ED VR AF-S DX Nikkor (08.01.2009)</li>
-<li>20mm f/2.8 D AF Nikkor (22.03.2012)</li>
-<li>28mm f/2.8 D AF Nikkor (14.06.1996)</li>
-<li>50mm f/1.8 AF Nikkor (11.04.1996)</li>
-<li>70-200mm f/2.8 G AF-S ED VR II (22.12.2011)</li>
-<li class="formerEquipment">80-400mm f/4.5-5.6D ED VR AF Zoom-Nikkor (27.10.2005 - 20.09.2013)</li>
-<li>105mm f/2.8 D AF Micro-Nikkor (14.06.1996)</li>
-<li>300mm f/2.8G AF-S Nikkor ED VR II (22.12.2011)</li>
-<li>600mm f/4G ED VR AF-S NIKKOR (27.07.2008)</li>
-<li class="formerEquipment">600mm f/5.6 MF Nikkor (14.03.1997 - 27.07.2008)</li>
+<li>Nikon 600mm f/4 FL ED VR (12.08.2015)</li>
+<li>Nikon 300mm f/2.8G AF-S ED VR II (22.12.2011)</li>
+<li>Nikon 105mm f/2.8 D AF (14.06.1996)</li>
+<li>Nikon 50mm f/1.8 AF (11.04.1996)</li>
+<li>Nikon 28mm f/2.8 D AF (14.06.1996)</li>
+<li>Nikon 24mm f/1.4G ED AF-S (26.03.2014)</li>
+<li>Nikon 20mm f/2.8 D AF (22.03.2012)</li>
+<li>Nikon 80-400mm f/4.5-5.6G ED VR AF-S (20.09.2013)</li>
+<li>Nikon 70-200mm f/2.8 G AF-S ED VR II (22.12.2011)</li>
 </ul>
 <h2>Accessories</h2>
 <ul class="main">
-<li>Tripod Ballhead <a href="http://reallyrightstuff.com/ProductDesc.aspx?code=BH-30-LR&type=3&eq=&desc=BH-30-LR%3a-Compact-ballhead-with-LR&key=it">BH-30-LR Really Right Stuff</a> (23.06.2012)</li>
-<li>Tripod Ballhead <a href="http://reallyrightstuff.com/ProductDesc.aspx?code=BH-55-LR&type=3&eq=&desc=BH-55-LR%3a-Full-sized-ballhead-with-LR&key=it">BH-55 LR Really Right Stuff</a> (21.06.2008)</li>
-<li>Tripod head by <a href="http://www.dietmar-nill.de/Stativkopf">Dietmar Nill</a></li>
-<li>Tripod <a href="http://www.gitzo.com/photo-tripods-systematic">Gitzo GT3541LS</a> (10.11.2008)</li>
-<li>Tripod <a href="http://www.gitzo.com/photo-tripods-systematic">Gitzo GT4552TS</a> (22.10.2012)</li>
-<li>Tripod <a href="http://www.gitzo.com/photo-tripods-systematic">Gitzo GT5541LS</a> (22.07.2008)</li>
-<li>Teleconverter TC-14E II (20.10.2008)</li>
-<li><a href="http://www.dawntech.hk/">Dawntec di-GPS Eco Pro-F M</a> (07.2014 - )</li>
-<li class="formerEquipment">Dawntec di-GPS Pro (17.06.2011)</li>
-<li class="formerEquipment">Solmeta Geotagger N2 (23.05.2009 - 2011)</li>
-<li>Solar panel <a href="http://www.sistech.com/products-9171-0/default.asp">CHEPRE 18V/18Wp</a> by SIStech (18.08.2012)</li>
-<li>Camera bag by Tenba <a href="http://www.tenba.com/Products/Vector-Top-Load-2-Krypton-Green.aspx">Vector Topload 2</a> (29.10.2012)</li>
-<li>Peli <a href="http://www.speich.net/articles/2013/05/10/best-way-to-transport-your-photo-equipment-including-the-600mm-vr/">Storm Case iM2875</a> (14.09.2012)</li>
-<li>Dia-/Filmscanner <a href="http://www.nikon.ch/de_CH/product/discontinued/film-scanners/2009/super-coolscan-5000">Nikon Super Coolscan 5000ED</a> (28.05.2004)</li>
-<li>Autofeed SF- 210</li>
+<li>Gimbal head <a href="http://www.jobu-design.com/Jobu-Heavy-Duty-MK-IV_p_126.html" target="_blank">Jobu Heavy Duty MK IV</a><br>
+For more information read my post <a href="/articles/en/2016/04/23/replacement-foot-for-the-nikon-600mm-fl/">Gimbal and replacement foot for the Nikon 600mm FL/</a></li>
+<li>Ballhead <a href="http://www.reallyrightstuff.com/Heads/Ballheads/BH-55-Full-Size-Ballhead" target="_blank">Really Right Stuff BH-55 LR</a> (21.06.2008)</li>
+<li>Ballhead <a href="http://www.reallyrightstuff.com/Heads/Ballheads/BH-30-Compact-Ballhead" target="_blank">Really Right Stuff BH-30-LR</a> (23.06.2012)</li>
+<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT5541LS</a> (22.07.2008)</li>
+<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT4552TS</a> (22.10.2012)</li>
+<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT3541LS</a> (10.11.2008)</li>
+<li>Teleconverter Nikon TC-14E II (20.10.2008)<br>
+For more information read my post <a href="/articles/en/2013/11/23/comparison-of-nikon-300mm-and-600mm-with-teleconverter-tc-14/">Comparison of Nikon 300mm and 600mm with Teleconverter TC-14</a></li>
+<li>GPS <a href="http://di-gps.com/shop/di-GPS-Eco-ProFessional-M-PF1-M" target="_blank">Dawntec di-GPS Eco ProF M</a> (07.2014)</li>
+<li>Solar panel <a href="http://www.sistech.com/products-9171-0/default.asp" target="_blank">SIStech CHEPRE 18V/18Wp</a> (18.08.2012)<br>
+For more information read my post <a href="/articles/en/2012/11/25/how-to-recharge-your-camera-batteries-in-the-amazonian-jungle/">How to recharge your camera batteries in the Amazonian jungle</a></li>
+<li>Camera bag <a href="http://www.tenba.com/Products/Vector-Top-Load-2-Krypton-Green.aspx" target="_blank">Tenba Vector Topload 2</a> (29.10.2012)</li>
+<li>Trunk <a href="http://www.peli.com/eu/en/product/watertight-storm-hard-cases/large-case/travel-case/iM2875/" target="_blank">Peli Storm Case iM2875</a> (14.09.2012)<br>
+For more information read my post <a href="/articles/en/2013/05/10/best-way-to-transport-your-photo-equipment-including-the-600mm-vr/">Best way to transport your photo equipment including a 600mm/f4 lens</a></li>
+<li>Slidescanner <a href="http://www.nikon.ch/de_CH/product/discontinued/film-scanners/2009/super-coolscan-5000" target="_blank">Nikon Super Coolscan 5000ED</a> (28.05.2004)</li>
+</ul>
+<h2>Former equipment</h2>
+<ul class="main">
+<li>Nikon D800 (21.03.2012)</li>
+<li>Nikon D300 (08.01.2009 - 18.08.2014)</li>
+<li>Nikon F90x (1996 - 2004)</li>
+<li>Nikon Coolpix P4 (19.05.2006 - 06.12.2013)</li>
+<li>Ricoh Singlex (- 2006)</li>
+<li>Nikon 600mm f/4G ED VR AF-S (27.07.2008)</li>
+<li>Nikon 600mm f/5.6 MF (14.03.1997 - 27.07.2008)</li>
+<li>Nikon 80-400mm f/4.5-5.6D ED VR AF (27.10.2005 - 20.09.2013)</li>
+<li>Nikon 18-200mm f/3.5-5.6 G ED VR AF-S (08.01.2009)</li>
+<li>GPS Dawntec di-GPS Pro (17.06.2011)</li>
+<li>GPS Solmeta Geotagger N2 (23.05.2009 - 2011)</li>
+<li>Gymbal head <a href="http://www.dietmar-nill.de/Stativkopf">Dietmar Nill</a></li>
 </ul>
 <?php require_once 'inc_body_end.php'; ?>
 </body>
