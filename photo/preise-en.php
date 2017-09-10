cf6828cf8b7cd1dc8fd5d790b772212235bfd680
Simon <simon.speich@gmail.com>
2017-09-10 19:29:36 +0200

    updated list of photography awards

--- photo/preise-en.php
+++ photo/preise-en.php
@@ -17,41 +17,47 @@ li img {
 <?php require_once 'inc_body_begin.php'; ?>
 <h1>Photography Awards</h1>
 <ul class="awards">
+<li><h3>NFS-Competition 2017</h3>
+Naturfotografen Schweiz, <a href="http://naturfotografen.ch/fotowettbewerb-2017.html" title="NFS-Wettbewerb 2017" target="_blank">NFS-Wettbewerb 2017</a><br>
+category birds, <strong>honorable mention</strong><br>
+<a href="photodb/images/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg"><img src="photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg" width="180" height="120" alt="photo of a crested tit"></a></li>
+<li>category landscape, <strong>honorable mention</strong><br>
+<a href="photodb/images/ch/ch012/ch012-031.jpg"><img src="photodb/images/thumbs/ch/ch012/ch012-031.jpg" width="180" height="119" alt="photo of iced over trees"></a></li>
 <li><h3>NFS-Competition 2016</h3>
-Winner, category Nature as Art<br>
 Naturfotografen Schweiz, <a href="http://naturfotografen.ch/id-2016.html" title="NFS-Wettbewerb 2016" target="_blank">NFS-Wettbewerb 2016</a><br>
-<a href="photodb/images/ch/ch014/ch014-078.jpg"><img src="photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="180" height="119" alt="Photo of frosted trees"></a></li>
+category Nature as Art, <strong>1. place</strong><br>
+<a href="photodb/images/ch/ch014/ch014-078.jpg"><img src="photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="180" height="119" alt="photo of frosted trees"></a></li>
 <li><h3>Glanzlichter 2016</h3>
-Highlight, category Black and White<br>
 projekt natur & fotografie, <a href="http://glanzlichter.com/template/index.cfm/fuseaction/executeMenuID/uuidMenu/7882AF21-F10E-77C3-B34D495D5BC2FAAA/lastuuid/7882AF21-F10E-77C3-B34D495D5BC2FAAA/23/index.htm" title="Glanzlichter 2016, 18. international Nature-Competition" target="_blank">Glanzlichter 2016</a><br>
-<a href="https://www.speich.net/articles/de/2016/04/23/1311/"><img src="images/helmkasuar.jpg" width="133" height="200" alt="Foto eines Helmkasuars"></a><br>
-Final round, category Wildlife in Human Environment<br>
+category Black and White, <strong>highlight</strong><br>
+<a href="https://www.speich.net/articles/de/2016/04/23/1311/"><img src="images/helmkasuar.jpg" width="133" height="200" alt="photo of a cassowary"></a><br>
+category Wildlife in Human Environment, <strong>final round</strong><br>
 <a href="https://speich/photo/photodb/photo-detail.php?imgId=4693"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="180" height="120"></a></li>
 <li><h3>Schnappschuss 2015/16</h3>
-Third, category Animal Portraits/Habitat/Behavior<br>
 Naturhistorisches Museum Basel, <a href="http://www.nmbs.ch/home/info_angebote/aktuell/schnappschuss.html" target="_blank">Schnappschuss</a><br>
-<a href="https://www.speich.net/articles/de/2016/03/11/3-rang-mit-foto-haubemeise/"><img src="images/2015-03-Pontresina-005.jpg" width="180" height="120" alt="Photo of a Crested Tit"></a></li>
+category Animal Portraits/Habitat/Behavior, <strong>3. place</strong><br>
+<a href="https://www.speich.net/articles/de/2016/03/11/3-rang-mit-foto-haubemeise/"><img src="images/2015-03-Pontresina-005.jpg" width="180" height="120" alt="photo of a Crested Tit"></a></li>
 <li><h3>GDT European Wildlife Photographer of the Year 2015</h3>
-Semifinal, category Man and Nature<br>
+category Man and Nature, <strong>semifinal</strong><br>
 <a href="https://speich/photo/photodb/photo-detail.php?imgId=4693"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="180" height="120"></a><br>
-Quarterfinal, category Landscapes<br>
-<a href="images/2012-10-Morteratsch-054.jpg"><img src="images/K5_Herbstwald.jpg" width="180" height="120" alt="Autumnal Larch Forest"></a>
+category Landscapes, <strong>quarterfinal</strong><br>
+<a href="images/2012-10-Morteratsch-054.jpg"><img src="images/K5_Herbstwald.jpg" width="180" height="120" alt="photo of a Autumnal Larch Forest"></a>
 </li>
 <li><h3>BILDERRAUSCH 2012/13</h3>
-Winner, category Animal Portraits/Habitat/Behavior<br>
 Naturhistorischen Museums Basel, <a href="http://www.nmb.bs.ch/wildlife/wildlife_fotowettbewerb.htm" target="_blank">BILDERRAUSCH</a><br>
-<a href="https://www.speich.net/articles/de/2013/02/21/fotowettbewerb-bilderrausch/"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="180" height="120" alt="Photo of a Reddish Egret"></a></li>
+category Animal Portraits/Habitat/Behavior, <strong>1. place</strong><br>
+<a href="https://www.speich.net/articles/de/2013/02/21/fotowettbewerb-bilderrausch/"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="180" height="120" alt="photo of a Reddish Egret"></a></li>
 <li><h3>photo.vogelwarte.ch 2012</h3>
-Finalist<br>
-Swiss Ornithological Institute in Sempach, <a href="http://photo.vogelwarte.ch/en/gallery-2012/#pic-67" alt="Finalist 2012" target="_blank">Photo contest</a><br>
-<a href="https://www.speich.net/photo/photodb/photo-detail.php?imgId=4702"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="180" height="120" alt="Photo of a Osprey with a fish in its claws"></a></li>
+Swiss Ornithological Institute in Sempach, <a href="http://photo.vogelwarte.ch/en/gallery-2012/#pic-67" alt="Finalist 2012" target="_blank">photo contest</a><br>
+<strong>finalist</strong><br>
+<a href="https://www.speich.net/photo/photodb/photo-detail.php?imgId=4702"><img src="photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="180" height="120" alt="photo of a Osprey with a fish in its claws"></a></li>
 <li><h3>WWF Magazin</h3>
-Second, reader photos <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 2/2012<br>
+reader photos <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 2/2012, <strong>2. place</strong><br>
 <a href="https://speich.net/photo/photodb/photo-detail.php?imgId=3252"><img src="photodb/images/thumbs/024/024-029.jpg" width="180" height="119"></a><br>
-Winner, reader photos <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 1/2012<br>
+reader photos <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 1/2012, <strong>1. place</strong><br>
 <a href="https://www.speich.net/photo/photodb/photo-detail.php?imgId=3066"><img src="photodb/images/thumbs/ch/ch011/ch011-033.jpg" width="119" height="180"></a></li>
 <li><h3>basellomorallye.02</h3>
-Winner, <a href="http://lomography.ch/2002/09/09/basellomorallye02/" target="_blank">basellomorallye 2002</a><br>
+<a href="http://lomography.ch/2002/09/09/basellomorallye02/" target="_blank">basellomorallye 2002</a>, <strong>1. place</strong><br>
 <a href="images/016-009.jpg"><img src="images/016-009-thumb.jpg" width="180" height="117" alt="hot love story"></a></li>
 </ul>
 <?php require_once 'inc_body_end.php'; ?>
