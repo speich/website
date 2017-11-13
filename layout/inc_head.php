ed6b05c6ab75eee08910f96a7ed61237a9c10b93
speich <info@speich.net>
2017-11-13 21:14:14 +0100

    set meta viewport for mobile

--- layout/inc_head.php
+++ layout/inc_head.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="/layout/layout.css" rel="stylesheet" type="text/css">
 <link href="<?php echo $web->getWebRoot().'layout/images/favicon.png'; ?>" type="image/png" rel="shortcut icon">
