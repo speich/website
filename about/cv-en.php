7aecebc74ffc68a597b3ecbc8943392142fcf890
Speich <simon.speich@wsl.ch>
2018-03-14 12:50:26 +0100

    updated links to NFI

--- about/cv-en.php
+++ about/cv-en.php
@@ -44,7 +44,7 @@ td:nth-child(2) {
 <td>05.2004—</td>
 <td></td>
 <td><p><strong>Web developer for the Swiss National Forest Inventory NFI</strong><br>
-	<a href="http://www.wsl.ch" target="_blank">Swiss Federal Institute for Forest, Snow and Landscape Research</a> WSL, Birmensdorf</p>
+	<a href="https://www.wsl.ch" target="_blank">Swiss Federal Institute for Forest, Snow and Landscape Research</a> WSL, Birmensdorf</p>
 <h3>Responsibilites:</h3>
 <ul class="main">
 <li>Concept, design and programming (PHP, JavaScript, SQL) of multilingual web products</li>
@@ -53,13 +53,13 @@ td:nth-child(2) {
 </ul>
 <h3>Projects:</h3>
 <ul class="main">
-<li>Web application to <a href="http://www.lfi.ch/resultate/anleitung-en.php" target="_blank">query and display results</a> (tables and maps) of the Swiss NFI</li>
+<li>Web application to <a href="https://www.lfi.ch/resultate/anleitung-en.php" target="_blank">query and display results</a> (tables and maps) of the Swiss NFI</li>
 <li>Web application: <a href="http://www.sciencedirect.com/science/article/pii/S016816991630792X" target="_blank">The data storage and analysis system of the Swiss National Forest Inventory</a><br>
 Computers and Electronics in Agriculture Volume 132</li>
-<li>Website of the <a href="http://www.lfi.ch/index.en.php" target="_blank">Swiss National Forest Inventory</a></li>
+<li>Website of the <a href="https://www.lfi.ch/index.en.php" target="_blank">Swiss National Forest Inventory</a></li>
 <li>Website with photo archive of the <a href="http://www.forstmuseum.ch" target="_blank">Förderverein Forstmuseum Ballenberg</a></li>
-<li>Photography and design of <a href="http://www.lfi.ch/publikationen/publ/LFI_Kalender_2014_de.pdf" target="_blank">NFI-calendar</a></li>
-<li>Redaction of <a href="http://www.lfi.ch/publikationen/publ_lfiinfo.php" target="_blank">LFI info</a> (discontinued)</li>
+<li>Photography and design of <a href="https://www.lfi.ch/publikationen/publ/kalender/LFI_Kalender_2018_de.pdf" target="_blank">NFI-calendar</a></li>
+<li>Redaction of <a href="https://www.lfi.ch/publikationen/publ_lfiinfo.php" target="_blank">LFI info</a> (discontinued)</li>
 </ul></td>
 </tr>
 <tr>
