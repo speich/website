a62458b813787da24f923e976cd834b29714c45c
Simon <simon.speich@gmail.com>
2018-04-01 16:12:23 +0200

    renamed variable

--- photo/photodb/photo-detail_inc.php
+++ photo/photodb/photo-detail_inc.php
@@ -5,21 +5,20 @@ use PhotoDb\PhotoDb;
 use WebsiteTemplate\Language;
 use WebsiteTemplate\Website;
 
+
 require_once __DIR__.'/../../library/inc_script.php';
 require_once 'photoinc.php';
 
 if (isset($_GET['imgId'])) {
 	$imgId = $_GET['imgId'];
-}
-else {
+} else {
 	header('Location: http://www.speich.net/photo/photodb/photo.php');
 }
 
-
 $ucLang = ucfirst($lang->get());
 // TODO: move date scanned out of Images?
-$db = new PhotoDb($web->getWebRoot());
-$db->connect();
+$photoDb = new PhotoDb($web->getWebRoot());
+$photoDb->connect();
 $sql = "SELECT I.Id imgId, I.ImgFolder imgFolder, I.ImgName imgName, I.ImgTechInfo imgTechInfo,
 	I.DateAdded dateAdded, I.LastChange lastChange, I.DatePublished datePublished, I.ImgDesc imgDesc, I.ImgTitle imgTitle, I.ImgDateOriginal imgDateOriginal,
 	I.ImgDateManual ImgDateManual,
@@ -58,18 +57,17 @@ $sql = "SELECT I.Id imgId, I.ImgFolder imgFolder, I.ImgName imgName, I.ImgTechIn
 	LEFT JOIN Countries C ON LC.CountryId = C.Id
 	LEFT JOIN Countries C2 ON I.CountryId = C2.Id 
 	WHERE I.Id = :imgId";
-$stmt = $db->db->prepare($sql);
+$stmt = $photoDb->db->prepare($sql);
 $stmt->bindValue(':imgId', $imgId);
 $stmt->execute();
 $photo = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $jsConfig = [
-    'lat' => empty($photo[0]['imgLat']) ? null : $photo[0]['imgLat'],
-    'lng' => empty($photo[0]['imgLng']) ? null : $photo[0]['imgLng']
+	'lat' => empty($photo[0]['imgLat']) ? null : $photo[0]['imgLat'],
+	'lng' => empty($photo[0]['imgLng']) ? null : $photo[0]['imgLng']
 ];
 $jsConfig = json_encode($jsConfig, JSON_NUMERIC_CHECK);
 $jsConfig = htmlspecialchars($jsConfig, ENT_COMPAT, $web->charset);
-
 
 /**
  * Print HTML to display photo detail.
@@ -79,25 +77,25 @@ $jsConfig = htmlspecialchars($jsConfig, ENT_COMPAT, $web->charset);
  * @param Language $lang
  * @param array $i18n internationalization
  */
-function renderPhoto($data, $db, $web, $lang, $i18n) {
-    $photo = new Photo($db->webroot);
+function renderPhoto($data, $db, $web, $lang, $i18n)
+{
+	$photo = new Photo($db->webroot);
 
 	$backPage = $lang->createPage('photo.php').$web->getQuery(['imgId'], 2);
 	if (strpos($backPage, $lang->createPage('photo-mapsearch.php')) !== false) {
-		$backPage = $lang->createPage('photo-mapsearch.php').'?'.$_SERVER['QUERY_STRING'];	// when coming from map via js lastPage was not set with latest query vars, use current
+		$backPage = $lang->createPage('photo-mapsearch.php').'?'.$_SERVER['QUERY_STRING'];    // when coming from map via js lastPage was not set with latest query vars, use current
 	}
 	$imgFile = $db->webroot.$db->getPath('img').$data['imgFolder'].'/'.$data['imgName'];
-    $dim = $photo->getImageSize($data);
+	$dim = $photo->getImageSize($data);
 	$star = '';
 
 	$str = '<img class="imgRatingStar" src="../../layout/images/ratingstar.gif" alt="star icon for rating image"/>';
 	for ($i = 0; $i < strlen($data['rating']); $i++) {
-		$star.= $str;
+		$star .= $str;
 	}
 	if ($data['dateTimeOriginal']) {
 		$datum = date("d.m.Y H:i:s", $data['dateTimeOriginal']);
-	}
-	else {
+	} else {
 		$datum = $data['ImgDateManual'];
 	}
 	echo '<h1>'.ucfirst($i18n['photo']).': '.$data['imgTitle']."</h1>\n";
@@ -120,7 +118,7 @@ function renderPhoto($data, $db, $web, $lang, $i18n) {
         <p class="photoTxtSeparator"><span class="photoTxtLabel">'.$i18n['rating'].':</span> '.$star.'</p>
         </div>';
 
-    echo '<div class="col colRight">';
+	echo '<div class="col colRight">';
 	echo '<div id="map">';
 	if ($data['showLoc'] === '0') {
 		echo '<div id="mapNote">'.$i18n['Coordinates are not shown'].'</div>';
@@ -140,12 +138,12 @@ function renderPhoto($data, $db, $web, $lang, $i18n) {
         <div class="col">
 	    <h3>'.$i18n['technical information'].' (Exif)</h3>';
 	if ($data['model'] == 'Nikon SUPER COOLSCAN 5000 ED') {
-	    echo '<ul><li><span class="photoTxtLabel">'.$i18n['type of film'].':</span> '.$data['film'].'</li>
+		echo '<ul><li><span class="photoTxtLabel">'.$i18n['type of film'].':</span> '.$data['film'].'</li>
 		    <li><span class="photoTxtLabel">'.$i18n['model'].': </span>'.$data['model'].', '.$data['make'].'</li></ul>';
-	}
-	else {
+	} else {
 		echo '<ul>
-            <li><span class="photoTxtLabel">'.$i18n['exposure'].':</span> '.$data['exposureTime']." at ƒ".number_format($data['fNumber'], 1).'
+            <li><span class="photoTxtLabel">'.$i18n['exposure'].':</span> '.$data['exposureTime']." at ƒ".number_format($data['fNumber'],
+				1).'
 		    <li><span class="photoTxtLabel">ISO:</span> '.$data['iso'].'</li>
 		    <li><span class="photoTxtLabel">'.$i18n['focal length'].':</span> '.$data['focalLength'].', '.$i18n['distance'].' : '.$data['focusDistance'].'</li>
 		    </ul>
@@ -157,7 +155,7 @@ function renderPhoto($data, $db, $web, $lang, $i18n) {
 	        <li><span class="photoTxtLabel">'.$i18n['model'].': </span>'.$data['model'].'</li>
 	        </ul>';
 	}
-    echo '<ul>
+	echo '<ul>
         <li><span class="photoTxtLabel">'.$i18n['position'].' (GPS):</span> '.($data['showLoc'] === '1' ? $data['gpsLatitude'].' / '.$data['gpsLongitude'] : '').'</li>
     	<li><span class="photoTxtLabel">'.$i18n['hight'].' (GPS):</span> '.$data['gpsAltitude'].' m '.($data['gpsAltitudeRef'] == '1' ? 'b.s.l.' : 'a.s.l.').'</li>
     	</ul>';
@@ -165,9 +163,12 @@ function renderPhoto($data, $db, $web, $lang, $i18n) {
 
 	echo '<div class="col">';
 	echo '<h3>'.$i18n['database information'].'</h3>';
-	echo '<ul><li><span class="photoTxtLabel">'.$i18n['added'].':</span> '.(!empty($data['dateAdded']) ? date("d.m.Y H:i:s", $data['dateAdded']) : '').'</li>
-	    <li><span class="photoTxtLabel">'.$i18n['changed'].':</span> '.(!empty($data['lastChange']) ? date("d.m.Y H:i:s", $data['lastChange']) : '').'</li>
-        <li><span class="photoTxtLabel">'.$i18n['published'].':</span> '.(!empty($data['lastChange']) ? date("d.m.Y H:i:s", $data['datePublished']) : '').'</li></ul>';
+	echo '<ul><li><span class="photoTxtLabel">'.$i18n['added'].':</span> '.(!empty($data['dateAdded']) ? date("d.m.Y H:i:s",
+			$data['dateAdded']) : '').'</li>
+	    <li><span class="photoTxtLabel">'.$i18n['changed'].':</span> '.(!empty($data['lastChange']) ? date("d.m.Y H:i:s",
+			$data['lastChange']) : '').'</li>
+        <li><span class="photoTxtLabel">'.$i18n['published'].':</span> '.(!empty($data['lastChange']) ? date("d.m.Y H:i:s",
+			$data['datePublished']) : '').'</li></ul>';
 	echo '<ul><li><span class="photoTxtLabel">'.$i18n['file format'].':</span> '.$data['fileType']." (".$data['fileSize'].')</li></ul>';
 	echo '</div></div>';
 	echo '<p><a href="'.$backPage.'">'.$i18n['back'].'</a></p>';
---
+++ test2.php
@@ -0,0 +1,17 @@
+<?php
+use PhotoDb\PhotoDb;
+
+require_once 'library/inc_script.php';
+require_once 'photo/photodb/scripts/php/PhotoDb.php';
+
+$db = new PhotoDb($web->getWebRoot());
+$db->connect();
+$sql = "DROP TABLE data";
+$db->db->exec($sql);
+$sql = "CREATE VIRTUAL TABLE data using fts4(keyword, tokenize=unicode61)";
+$db->db->exec($sql);
+var_dump($db->db->errorInfo());
+$db->db->exec("INSERT INTO data(keyword) VALUES ('Simon geht nach Hause einkaufen')");
+var_dump($db->db->errorInfo());
+$data = $db->db->query("SELECT * FROM data where keyword match 'haus'");
+var_dump($data->fetchAll());
