3d18fe1c46c4e600d86120d82eb27445a183464a
Simon <simon.speich@gmail.com>
2018-07-01 16:15:45 +0200

    updated equipment

--- photo/ausruestung-en.php
+++ photo/ausruestung-en.php
@@ -14,7 +14,6 @@
 <h2>Cameras</h2>
 <ul class="main">
 <li>Nikon D850 (19.04.2018)</li>
-<li>Nikon D810 (29.12.2014)</li>
 <li>Nikon D500 (15.12.2016)</li>
 <li>Nikon F100 (12.06.2004)</li>
 <li>Sony Cybershot RX100 (06.12.2013)</li>
@@ -54,18 +53,19 @@ For more information read my post <a href="/articles/en/2013/05/10/best-way-to-t
 </ul>
 <h2>Former equipment</h2>
 <ul class="main">
-<li>Nikon D800 (21.03.2012)</li>
-<li>Nikon D300 (08.01.2009 - 18.08.2014)</li>
-<li>Nikon F90x (1996 - 2004)</li>
-<li>Nikon Coolpix P4 (19.05.2006 - 06.12.2013)</li>
-<li>Ricoh Singlex (- 2006)</li>
-<li>Nikon 600mm f/4G ED VR AF-S (27.07.2008 - 30.03.2016)</li>
-<li>Nikon 600mm f/5.6 MF (14.03.1997 - 27.07.2008)</li>
-<li>Nikon 80-400mm f/4.5-5.6G ED VR AF-S (20.09.2013 - 20.05.2018)</li>
-<li>Nikon 80-400mm f/4.5-5.6D ED VR AF (27.10.2005 - 20.09.2013)</li>
-<li>Nikon 18-200mm f/3.5-5.6 G ED VR AF-S (08.01.2009 - 27.05.2014)</li>
+<li>Nikon D810 (29.12.2014–28.05.2018)</li>
+<li>Nikon D800 (21.03.2012–03.02.2017)</li>
+<li>Nikon D300 (08.01.2009–18.08.2014)</li>
+<li>Nikon F90x (1996–2004)</li>
+<li>Nikon Coolpix P4 (19.05.2006–06.12.2013)</li>
+<li>Ricoh Singlex (–2006)</li>
+<li>Nikon 600mm f/4G ED VR AF-S (27.07.2008–30.03.2016)</li>
+<li>Nikon 600mm f/5.6 MF (14.03.1997–27.07.2008)</li>
+<li>Nikon 80-400mm f/4.5-5.6G ED VR AF-S (20.09.2013–20.05.2018)</li>
+<li>Nikon 80-400mm f/4.5-5.6D ED VR AF (27.10.2005–20.09.2013)</li>
+<li>Nikon 18-200mm f/3.5-5.6 G ED VR AF-S (08.01.2009–27.05.2014)</li>
 <li>GPS Dawntec di-GPS Pro (17.06.2011)</li>
-<li>GPS Solmeta Geotagger N2 (23.05.2009 - 2011)</li>
+<li>GPS Solmeta Geotagger N2 (23.05.2009–2011)</li>
 <li>Gymbal head <a href="http://www.dietmar-nill.de/Stativkopf">Dietmar Nill</a></li>
 </ul>
 <?php require_once 'inc_body_end.php'; ?>
