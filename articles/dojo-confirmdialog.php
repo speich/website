e882fc3a480ea25bfb9a68be40ebe22d8034fd2a
Simon <simon.speich@gmail.com>
2018-07-20 22:16:15 +0200

    - use dojo 1.13.0 - include head (css, etc) - move var to scope start

--- articles/dojo-confirmdialog.php
+++ articles/dojo-confirmdialog.php
@@ -3,9 +3,8 @@
 <html lang="<?php echo $lang->get(); ?>">
 <head>
 <title><?php echo $web->pageTitle; ?>: dojo confirm dialog</title>
-<meta charset="utf-8">
-<link href="//ajax.googleapis.com/ajax/libs/dojo/1.11.1/dijit/themes/claro/claro.css" rel="stylesheet" type="text/css">
-<link href="../layout/layout.css" rel="stylesheet" type="text/css">
+<?php require_once '../layout/inc_head.php' ?>
+<link href="//ajax.googleapis.com/ajax/libs/dojo/1.13.0/dijit/themes/claro/claro.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 .dijitDialog {
 	width: 300px;
@@ -13,7 +12,9 @@
 .dialogConfirmButtons {
 	border-top: 1px solid #ccc;
 	padding-top: 3px;
-
+}
+.button {
+	padding: 2px 4px;
 }
 </style>
 </head>
@@ -37,7 +38,7 @@ var dojoConfig = {
 	]
 };
 </script>
-<script src="//ajax.googleapis.com/ajax/libs/dojo/1.11.1/dojo/dojo.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/dojo/1.13.0/dojo/dojo.js" type="text/javascript"></script>
 <script type="text/javascript">
 require([
 	'dojo/_base/array',
@@ -48,16 +49,18 @@ require([
 ], function(array, Deferred, dom, domConstruct, DialogConfirm) {
 
 	function startDemo() {
-		var dfds = [];
-		var arr = [0,1,2,3,4,5];
+		var dfds = [],
+			arr = [0,1,2,3,4,5];
 
 		dfds[0] = new Deferred();
 		dfds[0].resolve(false); // dummy to attach initial dfd to
 
 		array.forEach(arr, function(value, i) {
 			dfds[i + 1] = dfds[i].then(function(remember) {
+				var dfd,  dialog;
+
 				if (!remember) {
-					var dialog = new DialogConfirm({
+					dialog = new DialogConfirm({
 						title: 'Confirm dialog #' + i,
 						duration: 500,
 						content: '<p>I love dojo. I love dojo. I love dojo.</p>'
@@ -65,9 +68,10 @@ require([
 					return dialog.show();
 				}
 				else {
-					var dfd = new Deferred();
+					dfd = new Deferred();
 					dfd.resolve(true);
 					console.log('skipped');
+
 					return dfd;
 				}
 			});
@@ -83,8 +87,6 @@ require([
 		}
 	}, dom.byId('startLink'));
 });
-
-
 </script>
 <?php require_once 'inc_body_end.php'; ?>
 </body>
