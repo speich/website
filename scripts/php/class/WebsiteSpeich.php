55a9c8aa9d408dc8e73b38916145ca3c8a49844c
Simon <simon.speich@gmail.com>
2019-08-18 20:41:54 +0200

    add allowed domains

--- scripts/php/class/WebsiteSpeich.php
+++ scripts/php/class/WebsiteSpeich.php
@@ -2,6 +2,7 @@
 
 namespace speich;
 
+use WebsiteTemplate\Language;
 use WebsiteTemplate\Website;
 
 
@@ -9,6 +10,50 @@ class WebsiteSpeich extends Website
 {
     /** @var array keys that are allowed in the query string */
     protected $whitelistQueryString = ['pg', 'theme', 'country', 'qual', 'lang', 'sort', 'imgId', 'numRecPp', 'lat1', 'lat2', 'lng1', 'lng2'];
+
+    /** @var array whitelisted public domains */
+    private $domains = ['speich.ch', 'www.speich.ch'];
+
+    /** @var array whitelisted developer domains */
+    private $domainsDev = ['speich', 'localhost'];
+
+    /** @var Language */
+    public $lang;
+
+    /**
+     * Constructs the class.
+     * Sets default properties for language
+     */
+    public function __construct()
+    {
+        $domains = $this->getDomains('all');
+        parent::__construct($domains);
+    }
+
+    /**
+     * Returns the whitelisted domains.
+     * Returns the whitelisted domains by type 'public', 'dev' or 'all'
+     * @param string $type
+     * @return array
+     */
+    public function getDomains($type = 'public')
+    {
+        switch ($type) {
+            case 'dev':
+                $domains = $this->domainsDev;
+                break;
+            case 'all':
+                $domains = array_merge($this->domainsDev, $this->domains);
+                break;
+            case 'public':
+                $domains = $this->domains;
+                break;
+            default:
+                $domains = $this->domains;
+        }
+
+        return $domains;
+    }
 
     /**
      * @return array
