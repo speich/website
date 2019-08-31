4f3169ee55103b777c62529d4cba123fbdb44e25
Simon <simon.speich@gmail.com>
2019-08-31 16:51:24 +0200

    override language menu in case of wordpress virtual language directory

---
+++ scripts/php/class/LanguageMenu.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace speich;
+
+
+/**
+ * Class LanguageMenu
+ * @package speich
+ */
+class LanguageMenu extends \WebsiteTemplate\LanguageMenu
+{
+
+    /**
+     * Returns a HTML string with links to the current page in all available languages.
+     * Overrides rendering of menu links without checking if file exists on disk in case of virtual /articles/{lang}/... directory.
+     * Sets the new link directly to /articles/{newLang}/...
+     * @return string html
+     */
+    public function render(): string
+    {
+        $url = $_SERVER['REQUEST_URI'];
+        if (strpos($url, '/articles/') === false) {
+            return parent::render();
+        }
+        $language = $this->lang;
+        $str = '';
+        $str .= '<ul id="'.$this->ulId.'" class="'.$this->ulClass.'">';
+        foreach ($language->arrLang as $lang => $label) {
+            if ($lang === $language->get()) {
+                $str .= '<li class="'.$this->liClassActive.'">'.$label.'</a></li>';
+            }
+            else {
+                $str .= '<li><a href="/articles/'.$lang.'/">'.$label.'</a></li>';
+            }
+        }
+        $str .= '</ul>';
+
+        return $str;
+    }
+}
