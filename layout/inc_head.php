890695b1b15284c088214733185c53cae78b15e8
Simon <simon.speich@gmail.com>
2019-09-13 10:10:45 +0200

    use local font

--- layout/inc_head.php
+++ layout/inc_head.php
@@ -1,5 +1,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-regular.woff2" as="font">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-italic.woff2" as="font">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-600.woff2" as="font">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-600italic.woff2" as="font">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-700.woff2" as="font">
+<link rel="preload" href="/layout/fonts/open-sans-v17-latin-700italic.woff2" as="font">
 <link href="/layout/normalize.min.css" rel="stylesheet" type="text/css">
+<link href="/layout/format.css" rel="stylesheet" type="text/css">
 <link href="/layout/layout.css" rel="stylesheet" type="text/css">
 <link href="<?php echo $web->getWebRoot().'layout/images/favicon.png'; ?>" type="image/png" rel="shortcut icon">
