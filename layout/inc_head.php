f804799141915913f335b204a8ca3118b1c9e793
Simon <simon.speich@gmail.com>
2019-09-15 22:03:17 +0200

    use svg and text instead of gif/jpg for logo for nice scaling

--- layout/inc_head.php
+++ layout/inc_head.php
@@ -1,12 +1,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-regular.woff2" as="font">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-italic.woff2" as="font">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-600.woff2" as="font">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-600italic.woff2" as="font">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-700.woff2" as="font">
-<link rel="preload" href="/layout/fonts/open-sans-v17-latin-700italic.woff2" as="font">
-<link href="/layout/normalize.min.css" rel="stylesheet" type="text/css">
-<link href="/layout/format.css" rel="stylesheet" type="text/css">
-<link href="/layout/layout.css" rel="stylesheet" type="text/css">
-<link href="<?php echo $web->getWebRoot().'layout/images/favicon.png'; ?>" type="image/png" rel="shortcut icon">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-regular.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-italic.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-600.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-600italic.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-700.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-700italic.woff2" as="font">
+<link rel="preload" href="<?php echo $web->getWebRoot(); ?>layout/fonts/open-sans-v17-latin-800.woff2" as="font">
+<link href="<?php echo $web->getWebRoot(); ?>layout/normalize.min.css" rel="stylesheet" type="text/css">
+<link href="<?php echo $web->getWebRoot(); ?>layout/format.css" rel="stylesheet" type="text/css">
+<link href="<?php echo $web->getWebRoot(); ?>layout/layout.css" rel="stylesheet" type="text/css">
+<link href="<?php echo $web->getWebRoot(); ?>layout/images/favicon.png" type="image/png" rel="shortcut icon">
