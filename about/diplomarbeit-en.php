cdb43d81e4aeb2b4f77007e9fc7b5d780a79c55d
Simon <info@speich.net>
2019-10-04 12:47:58 +0200

    -removed unused css -use figure tag

--- about/diplomarbeit-en.php
+++ about/diplomarbeit-en.php
@@ -2,59 +2,56 @@
 <!DOCTYPE html>
 <html lang="<?php echo $lang->get(); ?>">
 <head>
-<title>Temporal and Spatial Pattern of Nocturnal Bird Migration | <?php echo $web->pageTitle; ?></title>
-<?php require_once '../layout/inc_head.php' ?>
-<style type="text/css">
-h2 { margin-bottom: 20px; }
-p img {
-	margin: 0 0 15px 15px;
-	border:  1px solid black;
-}
-</style>
-
+	<title>Temporal and Spatial Pattern of Nocturnal Bird Migration | <?php echo $web->pageTitle; ?></title>
+    <?php require_once '../layout/inc_head.php' ?>
 </head>
 
 <body>
 <?php require_once 'inc_body_begin.php'; ?>
 <h1>Diploma Thesis</h1>
 <h2>Temporal and Spatial Pattern of Nocturnal Bird Migration across the Western
-Mediterranean Sea Studied by Radar</h2>
+	Mediterranean Sea Studied by Radar</h2>
 <p><strong>General Introduction</strong></p>
-<p><img src="images/bruchwasserl_sw.jpg" align="right" alt="Foto Bruchwasserläufer"/>Migratory
-birds have to cross or circumvent two vast areas, the Sahara Desert and the Mediterreanean 
-Sea, during their migratory journey between their breeding areas in Europe and 
-their wintering areas in Africa. A lot of day migrants such as raptors, herons 
-and storks avoid crossing the Mediterranean Sea and concentrate at the Straits 
-of Gibraltar and Bosporus. But most birds migrate by night and until recently 
-relatively little was known about how and to what extent these night migrants 
-cross the Mediterranean Sea and the Sahara. </p>
-<p>It was supposed that they fly on a more or less broad front across the Mediterranean 
-and the Sahara during night and day (Moreau 1972, Curry-Lindahl 1981). Early radar 
-and visual observations gave no evidence for concentrations along the shortest 
-routes, such as Gibraltar, Sicily or Malta (Casement 1966) and many surveillance 
-radar studies suggested no deviations in tracks when birds flew across coastlines 
-(Eastwood 1967). In recent years however, field observations have shown that mountain 
-ridges can lead to local concentrations of the migratory stream (Bruderer 1996) 
-and that coasts can influence the flight behaviour of migrants (Alerstam, 1977). 
-It was argued that at least some of the birds maintained their temporal pattern 
-of flying by night and resting during the day even over a large ecological barrier 
-such as the Sahara (Biebach et al. 1986, Bairlein 1988). </p>
-<p>Therefore, the <a href="http://www.vogelwarte.ch">Swiss Ornithological 
-Institute</a> carried out a study supported by the <a href="http://www.snf.ch/">Swiss 
-National Science Foundation</a>, where bird migration across the western Mediterranean 
-was investigated in autumn 1996 and spring 1997. The aim of this study was to 
-find out if the the migratory stream occurs over a broad front across the Mediterranean 
-Sea and if islands such as the Balearic Islands, in particular the coastlines, 
-influence the migratory stream. Two tracking radars were operated, one on the 
-southern tip of the island Mallorca, the other near Malaga at the coast on the 
-Spanish mainland. Moon and infrared observations along the French and Spanish 
-coast provided additional information over a larger scale.</p>
-<p> This diploma thesis was part of the project. It focused on the temporal and 
-spatial pattern of nocturnal bird migration over Mallorca during the spring season 
-as recorded by radar. The thesis was divided into two themes which were analysed 
-and discussed in detail: </p>
+<p>
+	<figure><img src="images/bruchwasserl_sw.jpg" alt="Foto eines Bruchwasserläufers">
+		<figcaption>Wood sandpiper <span class="spec">(Tringa glareola)</span></figcaption>
+	</figure>
+	Migratory
+	birds have to cross or circumvent two vast areas, the Sahara Desert and the Mediterreanean
+	Sea, during their migratory journey between their breeding areas in Europe and
+	their wintering areas in Africa. A lot of day migrants such as raptors, herons
+	and storks avoid crossing the Mediterranean Sea and concentrate at the Straits
+	of Gibraltar and Bosporus. But most birds migrate by night and until recently
+	relatively little was known about how and to what extent these night migrants
+	cross the Mediterranean Sea and the Sahara.
+</p>
+<p>It was supposed that they fly on a more or less broad front across the Mediterranean
+	and the Sahara during night and day (Moreau 1972, Curry-Lindahl 1981). Early radar
+	and visual observations gave no evidence for concentrations along the shortest
+	routes, such as Gibraltar, Sicily or Malta (Casement 1966) and many surveillance
+	radar studies suggested no deviations in tracks when birds flew across coastlines
+	(Eastwood 1967). In recent years however, field observations have shown that mountain
+	ridges can lead to local concentrations of the migratory stream (Bruderer 1996)
+	and that coasts can influence the flight behaviour of migrants (Alerstam, 1977).
+	It was argued that at least some of the birds maintained their temporal pattern
+	of flying by night and resting during the day even over a large ecological barrier
+	such as the Sahara (Biebach et al. 1986, Bairlein 1988). </p>
+<p>Therefore, the <a href="http://www.vogelwarte.ch">Swiss Ornithological
+		Institute</a> carried out a study supported by the <a href="http://www.snf.ch/">Swiss
+		National Science Foundation</a>, where bird migration across the western Mediterranean
+	was investigated in autumn 1996 and spring 1997. The aim of this study was to
+	find out if the the migratory stream occurs over a broad front across the Mediterranean
+	Sea and if islands such as the Balearic Islands, in particular the coastlines,
+	influence the migratory stream. Two tracking radars were operated, one on the
+	southern tip of the island Mallorca, the other near Malaga at the coast on the
+	Spanish mainland. Moon and infrared observations along the French and Spanish
+	coast provided additional information over a larger scale.</p>
+<p> This diploma thesis was part of the project. It focused on the temporal and
+	spatial pattern of nocturnal bird migration over Mallorca during the spring season
+	as recorded by radar. The thesis was divided into two themes which were analysed
+	and discussed in detail: </p>
 <p>1) the temporal and seasonal schedule of the migration intensities and<br/>
-2) the direction, speed, and potential recruiting areas of the migrants.</p>
+	2) the direction, speed, and potential recruiting areas of the migrants.</p>
 <p>The thesis can be <a href="diplomarbeit.pdf">downloaded as a pdf</a> (1.6 MB).</p>
 <?php require_once 'inc_body_end.php'; ?>
 </body>
