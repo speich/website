63b3509dd076bcc56fa445807f3959300ad40851
Simon <simon.speich@gmail.com>
2020-06-13 11:06:03 +0200

    English pages should have same code as German pages

--- photo/photodb/photo-mapsearch-en.php
+++ photo/photodb/photo-mapsearch-en.php
@@ -6,29 +6,27 @@ $web->setLastPage();
 <!DOCTYPE html>
 <html lang="en-us">
 <head>
-<title><?php echo $i18n['page title'].' | '.$web->pageTitle; ?></title>
-<?php require_once 'inc_head.php' ?>
-<link href="photodb.min.css" rel="stylesheet" type="text/css">
-<link href="photo-mapsearch.min.css" rel="stylesheet" type="text/css">
+    <title><?php echo $i18n['page title'].' | '.$web->pageTitle; ?></title>
+    <?php require_once 'inc_head.php' ?>
+    <link href="photodb.min.css" rel="stylesheet" type="text/css">
+    <link href="photo-mapsearch.min.css" rel="stylesheet" type="text/css">
 </head>
 
-<body class="tundra">
+<body>
 <?php require_once 'inc_body_begin.php'; ?>
-<div id="loading"><img src="../../layout/images/icon_loading.gif"><span id="loadingMsg"><?php echo $i18n['loading map']; ?></span></div>
 <div id="mapContainer">
-<?php echo $mRating->render(); ?>
-<div id="showPhotos" class="button buttShowPhotos" title="<?php echo $i18n['show photos']; ?>"><a href="photo.php" id="linkShowPhotos"><?php echo $i18n['photos']; ?><img src="../../layout/images/icon_photolist.gif" alt="icon to display list of photos"></a></div>
-<div id="map-canvas"></div>
+    <div id="map-canvas"></div>
+</div>
+<div class="toolbar hidden">
+    <?php echo $mRating->render(); ?>
+    <button id="showPhotos" class="bar-item"><a href="photo.php"><?php echo $i18n['photos']; ?>
+            <svg class="icon">
+                <use xlink:href="../../layout/images/symbols.svg#grid">
+            </svg>
+        </a></button>
 </div>
 
 <?php require_once 'inc_body_end.php'; ?>
-<script type="text/javascript" src="../../library/dojo/1.13.0/dojo/dojo.js" data-dojo-config="async: true,
-	gmapsApiKey: 'AIzaSyBEPhZpv_OQYeJH_mVYEOubDgGLlY5aLWg',
-	locale: '<?php echo $locale = $lang->get(); ?>',
-	packages: [
-		{name: 'gmap', location: './../../../gmap'}
-	]"></script>
-<script type="text/javascript" src="photo-mapsearch.min.js">
-</script>
+<script src="photo-mapsearch.min.js" type="module"></script>
 </body>
 </html>
--- photo/photodb/photo-mapsearch.php
+++ photo/photodb/photo-mapsearch.php
@@ -1,24 +1,29 @@
 <?php
-
 require_once 'photo_inc.php';
 $i18n = require_once __DIR__.'/nls/'.$lang->get().'/photo-mapsearch.php';
 $web->setLastPage();
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $lang->get(); ?>">
+<html lang="de-ch">
 <head>
-<title><?php echo $i18n['page title'].' | '.$web->pageTitle; ?></title>
-<?php require_once 'inc_head.php' ?>
-<link href="photodb.min.css" rel="stylesheet" type="text/css">
-<link href="photo-mapsearch.min.css" rel="stylesheet" type="text/css">
+    <title><?php echo $i18n['page title'].' | '.$web->pageTitle; ?></title>
+    <?php require_once 'inc_head.php' ?>
+    <link href="photodb.min.css" rel="stylesheet" type="text/css">
+    <link href="photo-mapsearch.min.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <?php require_once 'inc_body_begin.php'; ?>
-<div id="mapContainer"><div id="map-canvas"></div></div>
+<div id="mapContainer">
+    <div id="map-canvas"></div>
+</div>
 <div class="toolbar hidden">
-<?php echo $mRating->render(); ?>
-<button id="showPhotos" class="bar-item"><a href="photo.php"><?php echo $i18n['photos']; ?><svg class="icon"><use xlink:href="../../layout/images/symbols.svg#grid"></svg></a></button>
+    <?php echo $mRating->render(); ?>
+    <button id="showPhotos" class="bar-item"><a href="photo.php"><?php echo $i18n['photos']; ?>
+            <svg class="icon">
+                <use xlink:href="../../layout/images/symbols.svg#grid">
+            </svg>
+        </a></button>
 </div>
 
 <?php require_once 'inc_body_end.php'; ?>
