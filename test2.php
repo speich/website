bd014586d73dee5c9383a2fcb8fb45c7aebb178e
Simon <simon.speich@gmail.com>
2020-06-30 11:16:01 +0200

    added new rewards 2020

--- test2.php
+++ test2.php
@@ -1,18 +1,32 @@
 <?php
-use PhotoDb\PhotoDb;
 
-require_once 'scripts/php/inc_script.php';
-require_once 'photo/photodb/scripts/php/PhotoDb.php';
-
+echo "SQLite v:".SQLite3::version()['versionString']."<br>";
 // Test checks for unicode61 support in fts4
-$db = new PhotoDb($web->getWebRoot());
-$db->connect();
-$sql = "DROP TABLE data";
-$db->db->exec($sql);
-$sql = "CREATE VIRTUAL TABLE data using fts4(keyword, tokenize=unicode61)";
-$db->db->exec($sql);
-var_dump($db->db->errorInfo());
-$db->db->exec("INSERT INTO data(keyword) VALUES ('Simon geht nach Hause einkaufen')");
-var_dump($db->db->errorInfo());
-$data = $db->db->query("SELECT * FROM data where keyword match 'Haus'");
+try {
+	$db = new PDO('sqlite:test.sqlite');
+}
+catch (PDOException $error) {
+	echo $error->getMessage();
+}
+
+$db->exec("DROP TABLE data");
+//$sql = "CREATE VIRTUAL TABLE data USING fts5(keyword)";
+$sql = "CREATE VIRTUAL TABLE data USING fts4(keyword, tokenize=unicode61)";
+//$sql = "CREATE VIRTUAL TABLE data USING fts4(keyword)";
+try {
+    $db->exec($sql);
+    echo "1: ";
+    var_dump($db->errorInfo());
+    echo "<br>";
+}
+catch (PDOException $error) {
+	echo "2: ".$error->getMessage();
+    echo "<br>";
+}
+
+$db->exec("INSERT INTO data(keyword) VALUES ('Simon gefallen die Wälder im Jura')");
+echo "3: ";
+var_dump($db->errorInfo());
+echo "<br>4: ";
+$data = $db->query("SELECT * FROM data where keyword match 'wald*'");
 var_dump($data->fetchAll());
