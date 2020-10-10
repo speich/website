86236fdb9218838c31858ecce39c9c8c679634f7
Simon <simon.speich@gmail.com>
2020-10-10 13:57:32 +0200

    use setter instead of direct property access

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -21,14 +21,14 @@ $lang->arrLang = ['de' => 'Deutsch', 'en' => 'English'];
 $lang->autoSet();
 
 $web = new WebsiteSpeich();
-$web->lastUpdate = '30.06.2020';
+$web->setLastUpdate('2020-10-10');
 $web->setWebroot('/');
 ini_set('default_charset', $web->charset);
 $isPhoto = strpos($_SERVER['REQUEST_URI'], '/photo') !== false;
 if ($lang->get() === 'de') {
     $windowTitle = 'Fotografie und Webprogrammierung';
-    $htmlFooter['de'] = ($isPhoto ? '<div>' : '').'<p>© 2003-2019 speich.net, Konzept und Programmierung Simon Speich</p>';
-    $htmlFooter['de'] .= '<p class="last-update">letzte Aktualisierung '.$web->lastUpdate.'</p>'.($isPhoto ? '</div>' : '');
+    $htmlFooter['de'] = ($isPhoto ? '<div>' : '').'<p>© 2003-2020 speich.net, Konzept und Programmierung Simon Speich</p>';
+    $htmlFooter['de'] .= '<p class="last-update">letzte Aktualisierung '.$web->getLastUpdate().'</p>'.($isPhoto ? '</div>' : '');
     if ($isPhoto) {
         $htmlFooter['de'] .= '<p><a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.de"><img alt="Creative Commons Lizenzvertrag" src="https://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"></a>
         Alle Fotos stehen unter der <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.de">Creative Commons Lizenz</a> zur Verfügung,
@@ -39,8 +39,8 @@ if ($lang->get() === 'de') {
 }
 else {
 	$windowTitle = 'Photography and web programming';
-    $htmlFooter['en'] = ($isPhoto ? '<div>' : '').''.'<p>© 2003-2019 speich.net, concept und programming Simon Speich</p>';
-    $htmlFooter['en'] .= '<p class="last-update">last update '.$web->lastUpdate.'</p>'.($isPhoto ? '</div>' : '');
+    $htmlFooter['en'] = ($isPhoto ? '<div>' : '').''.'<p>© 2003-2020 speich.net, concept und programming Simon Speich</p>';
+    $htmlFooter['en'] .= '<p class="last-update">last update '.$web->getLastUpdate().'</p>'.($isPhoto ? '</div>' : '');
     if ($isPhoto) {
         $htmlFooter['en'] .= '<p><a rel="license" href="https://creativecommons.org/licenses/by-nc/3.0/"><img alt="Creative Commons licence" src="https://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"></a>
         All photos on this website are licenced under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution 3.0 Unported License</a>:<br>
