6120eea27883a4caac9786968c865787d9f6e86f
Simon <simon.speich@gmail.com>
2021-05-14 14:44:54 +0200

    use class instead of include

--- wp/wp-content/themes/twentynineteen-child/functions.php
+++ wp/wp-content/themes/twentynineteen-child/functions.php
@@ -2,15 +2,24 @@
 require_once __DIR__.'/../../../../scripts/php/inc_script.php';
 
 // remember: in wordpress head footer are included in function scope, so variables can not be shared directly
-add_action('wp_head', static function() use ($web) {
-    require_once 'inc_head.php';
+add_action('wp_head', static function() use ($head) {
+    echo $head->render();
 });
-add_action('wp_body_open', static function() use($lang, $web, $mainNav, $sideNav, $langNav) {
-    require_once 'inc_body_begin.php';
+add_action('wp_body_open', static function() use($bodyStart, $mainNav, $sideNav, $langNav) {
+    $str = $bodyStart->render($mainNav, $sideNav, $langNav);
+    // TODO: find better solution
+    $str = str_replace('<main>',   '', $str);
+    $str = preg_replace('/<nav class="sub">(.|\s)*?<\/nav>/', '', $str);
+    echo $str;
+    echo '<nav class="sub">';
+    get_template_part('template-parts/content/sidemenu');
+    echo '</nav>';
+    echo '<main>';
 });
-add_action('wp_footer', static function() use ($htmlFooter, $lang) {
-    require_once 'inc_body_end.php';
+add_action('wp_footer', static function() use ($bodyEnd) {
+    echo $bodyEnd->render();
 });
+
 add_action('wp_enqueue_scripts', static function() {
     wp_enqueue_style('style', get_stylesheet_directory_uri().'/style2.min.css');
     wp_enqueue_style('prismjs', '/../library/prismjs-1.20.0/prism.min.css');
