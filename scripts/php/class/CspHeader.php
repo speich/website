63fca427818c6dba1012c92705f796d369a424c4
Simon <simon.speich@gmail.com>
2021-10-29 11:09:16 +0200

    added new class to set a CSP header

---
+++ scripts/php/class/CspHeader.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace speich;
+
+/**
+ * Class CspHeader
+ * Check header with https://cspscanner.com
+ * or https://csp-evaluator.withgoogle.com/
+ */
+class CspHeader
+{
+    private static array $policies = [
+        'default-src' => "'self'",
+        'script-src' => "'self'",
+        'img-src' => "'self'",
+        'style-src' => "'self'",
+        'base-uri' => "'self'",
+        'form-action' => "'self'",
+        'frame-ancestors' => "'none'",
+        'frame-src' => "'none'",
+        'child-src' => "'none'",
+        'object-src' => "'none'",
+        'worker-src' => "'none'"
+    ];
+
+    /**
+     * @param string $directive
+     * @param string $value
+     */
+    public function set(string $directive, string $value): void
+    {
+        self::$policies[$directive] = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function toString(): string
+    {
+        $str = '';
+        foreach (self::$policies as $directive => $value) {
+            $str.= $directive.' '.$value.';';
+        }
+
+        return 'Content-Security-Policy: '.$str;
+    }
+}
