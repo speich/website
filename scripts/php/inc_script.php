a0b825ca69fd3a45f5cd7d4e446931add08093ad
Simon <simon.speich@gmail.com>
2021-10-29 11:04:26 +0200

    add hsts header

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -18,7 +18,7 @@ $language->arrLang = ['de' => 'Deutsch', 'en' => 'English'];
 $language->autoSet();
 
 $web = new WebsiteSpeich();
-$web->setLastUpdate('2021-09-04');
+$web->setLastUpdate('2021-10-29');
 $web->setWebroot('/');
 ini_set('default_charset', $web->charset);
 $windowTitle = $language->get() === 'de' ? 'Fotografie und Webprogrammierung' : 'Photography and web programming';
@@ -26,6 +26,7 @@ $web->pageTitle = 'Simon Speich - '.$windowTitle;
 
 require_once __DIR__.'/../../layout/inc_nav.php';
 
+header('Strict-Transport-Security: max-age=63072000; includeSubdomains; preload');
 $head = new Head($web->getWebRoot());
 $bodyStart = new BodyStart($web->getWebRoot(), $language);
 $bodyEnd = new BodyEnd($web, $language);
