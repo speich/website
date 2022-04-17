c0dd65b3c7ffe846522332d3eed2dc88cae815da
Simon <simon.speich@gmail.com>
2022-04-17 18:43:15 +0200

    update list of equipment and outdated links

--- photo/ausruestung-en.php
+++ photo/ausruestung-en.php
@@ -15,6 +15,9 @@
 	</figcaption></figure>
 <h2>Cameras</h2>
 <ul class="main">
+<li>Nikon Z9 (27.12.2021)</li>
+</ul>
+<ul class="main">
 <li>Nikon D850 (19.04.2018)</li>
 <li>Nikon D500 (15.12.2016)</li>
 <li>Nikon F100 (12.06.2004)</li>
@@ -23,24 +26,31 @@
 </ul>
 <h2>Lenses</h2>
 <ul class="main">
+<li>Nikon Z 70-200mm f/2.8 VR S (11.01.2022)</li>
+<li>Nikon Z 24mm f/1.8 S (11.01.2022)</li>
+</ul>
+<ul class="main">
 <li>Nikon 600mm f/4E FL ED VR (12.08.2015)</li>
 <li>Nikon 500mm f/5.6E PF ED VR 20.02.2020</li>
 <li>Nikon 300mm f/4G AF-S PF ED VR (19.04.2018)</li>
 <li>Nikon 300mm f/2.8G AF-S ED VR II (22.12.2011)</li>
 <li>Nikon 105mm f/2.8D AF (14.06.1996)</li>
+<li>Nikon 70-200mm f/2.8G AF-S ED VR II (22.12.2011)</li>
 <li>Nikon 50mm f/1.8 AF (11.04.1996)</li>
 <li>Nikon 28mm f/2.8D AF (14.06.1996)</li>
 <li>Nikon 24mm f/1.4G ED AF-S (26.03.2014)</li>
 <li>Nikon 20mm f/2.8D AF (22.03.2012)</li>
-<li>Nikon 70-200mm f/2.8G AF-S ED VR II (22.12.2011)</li>
 <li>Nikon 16-80mm f/2.8-4E AF-S ED VR (27.04.2018)</li>
 </ul>
 <h2>Accessories</h2>
 <ul class="main">
-<li>Gimbal head <a href="http://www.jobu-design.com/Jobu-Heavy-Duty-MK-IV_p_126.html" target="_blank">Jobu Heavy Duty MK IV</a><br>
+<li>Nikon FTZ II mount adapter (01.12.2021)</li>
+</ul>
+<ul class="main">
+<li>Gimbal head <a href="https://www.jobu-design.com/product-page/jobu-heavy-duty-mk-iv" target="_blank">Jobu Heavy Duty MK IV</a> (25.04.2016)<br>
 For more information read my post <a href="/articles/en/2016/04/23/replacement-foot-for-the-nikon-600mm-fl/">Gimbal and replacement foot for the Nikon 600mm FL/</a></li>
-<li>Ballhead <a href="http://www.reallyrightstuff.com/Heads/Ballheads/BH-55-Full-Size-Ballhead" target="_blank">Really Right Stuff BH-55 LR</a> (21.06.2008)</li>
-<li>Ballhead <a href="http://www.reallyrightstuff.com/Heads/Ballheads/BH-30-Compact-Ballhead" target="_blank">Really Right Stuff BH-30-LR</a> (23.06.2012)</li>
+<li>Ballhead <a href="https://www.reallyrightstuff.com/bh55-ball-head" target="_blank">Really Right Stuff BH-55 LR</a> (21.06.2008)</li>
+<li>Ballhead <a href="https://www.reallyrightstuff.com/bh30-ball-head" target="_blank">Really Right Stuff BH-30-LR</a> (23.06.2012)</li>
 <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT5541LS</a> (22.07.2008)</li>
 <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT4552TS</a> (22.10.2012)</li>
 <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT3541LS</a> (10.11.2008)</li>
@@ -49,7 +59,7 @@ For more information read my post <a href="/articles/en/2013/11/23/comparison-of
 <li>GPS <a href="http://di-gps.com/shop/di-GPS-Eco-ProFessional-M-PF1-M" target="_blank">Dawntec di-GPS Eco ProF M</a> (07.2014)</li>
 <li>Solar panel <a href="http://www.sistech.com/products-9171-0/default.asp" target="_blank">SIStech CHEPRE 18V/18Wp</a> (18.08.2012)<br>
 For more information read my post <a href="/articles/en/2012/11/25/how-to-recharge-your-camera-batteries-in-the-amazonian-jungle/">How to recharge your camera batteries in the Amazonian jungle</a></li>
-<li>Camera bag <a href="http://www.tenba.com/Products/Vector-Top-Load-2-Krypton-Green.aspx" target="_blank">Tenba Vector Topload 2</a> (29.10.2012)</li>
+<li>Camera bag <a href="https://www.tenba.com/" target="_blank">Tenba Vector Topload 2</a> (29.10.2012)</li>
 <li>Trunk <a href="http://www.peli.com/eu/en/product/watertight-storm-hard-cases/large-case/travel-case/iM2875/" target="_blank">Peli Storm Case iM2875</a> (14.09.2012)<br>
 For more information read my post <a href="/articles/en/2013/05/10/best-way-to-transport-your-photo-equipment-including-the-600mm-vr/">Best way to transport your photo equipment including a 600mm/f4 lens</a></li>
 <li>Slidescanner <a href="http://www.nikon.ch/de_CH/product/discontinued/film-scanners/2009/super-coolscan-5000" target="_blank">Nikon Super Coolscan 5000ED</a> (28.05.2004)</li>
@@ -71,7 +81,7 @@ For more information read my post <a href="/articles/en/2013/05/10/best-way-to-t
 <li>Teleconverter Nikon TC-14E II (20.10.2008)<br>
 <li>GPS Dawntec di-GPS Pro (17.06.2011)</li>
 <li>GPS Solmeta Geotagger N2 (23.05.2009–2011)</li>
-<li>Gymbal head <a href="http://www.dietmar-nill.de/Stativkopf">Dietmar Nill</a></li>
+<li>Gymbal head <a href="https://www.dietmar-nill.de/content/Stativkopf.html">Dietmar Nill</a></li>
 </ul>
 <?php echo $bodyEnd->render(); ?>
 </body>
