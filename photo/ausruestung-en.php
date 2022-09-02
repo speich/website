577f355c96fbe84b59462ac7db87133c0d32d62b
Simon <simon.speich@gmail.com>
2022-09-02 11:08:10 +0200

    update list of equipment

--- photo/ausruestung-en.php
+++ photo/ausruestung-en.php
@@ -2,86 +2,95 @@
 <!DOCTYPE html>
 <html lang="<?php echo $language->get(); ?>">
 <head>
-<title>Equipment | <?php echo $web->pageTitle; ?></title>
-<?php echo $head->render(); ?>
+    <title>Equipment | <?php echo $web->pageTitle; ?></title>
+    <?php echo $head->render(); ?>
 </head>
 
 <body>
 <?php echo $bodyStart->render($mainNav, $sideNav, $langNav); ?>
 <h1>Photo Equipment</h1>
 <figure class="photoContainer"><img src="images/guyana-simon.jpg" alt="Photo of Simon Speich" title="Simon with Nikon 300mm in Action"
-											class="imgFrame" style="background-image: url(images/guyana-simon.jpg)">
-	<figcaption>Simon taking a photo of a <a href="photodb/photo-detail.php?country=12&numRec=32&imgId=4971">Harpy Eagle</a> in Guyana's rainforest.
-	</figcaption></figure>
+            class="imgFrame" style="background-image: url(images/guyana-simon.jpg)">
+    <figcaption>Simon taking a photo of a <a href="photodb/photo-detail.php?country=12&numRec=32&imgId=4971">Harpy Eagle</a> in Guyana's rainforest.
+    </figcaption>
+</figure>
 <h2>Cameras</h2>
 <ul class="main">
-<li>Nikon Z9 (27.12.2021)</li>
+    <li>Nikon Z9 (27.12.2021)</li>
 </ul>
 <ul class="main">
-<li>Nikon D850 (19.04.2018)</li>
-<li>Nikon D500 (15.12.2016)</li>
-<li>Nikon F100 (12.06.2004)</li>
-<li>Sony Cybershot RX100 VA (06.10.2019)</li>
-<li>Minox 35 GL </li>
+    <li>Nikon D850 (19.04.2018)</li>
+    <li>Nikon D500 (15.12.2016)</li>
+    <li>Nikon F100 (12.06.2004)</li>
+    <li>Sony Cybershot RX100 VA (06.10.2019)</li>
+    <li>Minox 35 GL</li>
 </ul>
 <h2>Lenses</h2>
 <ul class="main">
-<li>Nikon Z 70-200mm f/2.8 VR S (11.01.2022)</li>
-<li>Nikon Z 24mm f/1.8 S (11.01.2022)</li>
+    <li>Nikkor Z MC 105mm 1:2,8 VR S (18.05.2022)</li>
+    <li>Nikkor Z 70-200mm f/2.8 VR S (11.01.2022)</li>
+    <li>Nikkor Z 24mm f/1.8 S (11.01.2022)</li>
 </ul>
 <ul class="main">
-<li>Nikon 600mm f/4E FL ED VR (12.08.2015)</li>
-<li>Nikon 500mm f/5.6E PF ED VR 20.02.2020</li>
-<li>Nikon 300mm f/4G AF-S PF ED VR (19.04.2018)</li>
-<li>Nikon 300mm f/2.8G AF-S ED VR II (22.12.2011)</li>
-<li>Nikon 105mm f/2.8D AF (14.06.1996)</li>
-<li>Nikon 70-200mm f/2.8G AF-S ED VR II (22.12.2011)</li>
-<li>Nikon 50mm f/1.8 AF (11.04.1996)</li>
-<li>Nikon 28mm f/2.8D AF (14.06.1996)</li>
-<li>Nikon 24mm f/1.4G ED AF-S (26.03.2014)</li>
-<li>Nikon 20mm f/2.8D AF (22.03.2012)</li>
-<li>Nikon 16-80mm f/2.8-4E AF-S ED VR (27.04.2018)</li>
+    <li>Nikkor 600mm f/4E FL ED VR (12.08.2015)</li>
+    <li>Nikkor 500mm f/5.6E PF ED VR 20.02.2020</li>
+    <li>Nikkor 300mm f/4G AF-S PF ED VR (19.04.2018)</li>
+    <li>Nikkor 300mm f/2.8G AF-S ED VR II (22.12.2011)</li>
+    <li>Nikkor 105mm f/2.8D AF (14.06.1996)</li>
+    <li>Nikkor 70-200mm f/2.8G AF-S ED VR II (22.12.2011)</li>
+    <li>Nikkor 50mm f/1.8 AF (11.04.1996)</li>
+    <li>Nikkor 28mm f/2.8D AF (14.06.1996)</li>
+    <li>Nikkor 24mm f/1.4G ED AF-S (26.03.2014)</li>
+    <li>Nikkor 20mm f/2.8D AF (22.03.2012)</li>
+    <li>Nikkor 16-80mm f/2.8-4E AF-S ED VR (27.04.2018)</li>
 </ul>
 <h2>Accessories</h2>
 <ul class="main">
-<li>Nikon FTZ II mount adapter (01.12.2021)</li>
+    <li>Nikon FTZ II mount adapter (01.12.2021)</li>
 </ul>
 <ul class="main">
-<li>Gimbal head <a href="https://www.jobu-design.com/product-page/jobu-heavy-duty-mk-iv" target="_blank">Jobu Heavy Duty MK IV</a> (25.04.2016)<br>
-For more information read my post <a href="/articles/en/2016/04/23/replacement-foot-for-the-nikon-600mm-fl/">Gimbal and replacement foot for the Nikon 600mm FL/</a></li>
-<li>Ballhead <a href="https://www.reallyrightstuff.com/bh55-ball-head" target="_blank">Really Right Stuff BH-55 LR</a> (21.06.2008)</li>
-<li>Ballhead <a href="https://www.reallyrightstuff.com/bh30-ball-head" target="_blank">Really Right Stuff BH-30-LR</a> (23.06.2012)</li>
-<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT5541LS</a> (22.07.2008)</li>
-<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT4552TS</a> (22.10.2012)</li>
-<li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT3541LS</a> (10.11.2008)</li>
-<li>Teleconverter Nikon TC-14E III (16.02.2020)<br>
-For more information read my post <a href="/articles/en/2013/11/23/comparison-of-nikon-300mm-and-600mm-with-teleconverter-tc-14/">Comparison of Nikon 300mm and 600mm with Teleconverter TC-14</a></li>
-<li>GPS <a href="http://di-gps.com/shop/di-GPS-Eco-ProFessional-M-PF1-M" target="_blank">Dawntec di-GPS Eco ProF M</a> (07.2014)</li>
-<li>Solar panel <a href="http://www.sistech.com/products-9171-0/default.asp" target="_blank">SIStech CHEPRE 18V/18Wp</a> (18.08.2012)<br>
-For more information read my post <a href="/articles/en/2012/11/25/how-to-recharge-your-camera-batteries-in-the-amazonian-jungle/">How to recharge your camera batteries in the Amazonian jungle</a></li>
-<li>Camera bag <a href="https://www.tenba.com/" target="_blank">Tenba Vector Topload 2</a> (29.10.2012)</li>
-<li>Trunk <a href="http://www.peli.com/eu/en/product/watertight-storm-hard-cases/large-case/travel-case/iM2875/" target="_blank">Peli Storm Case iM2875</a> (14.09.2012)<br>
-For more information read my post <a href="/articles/en/2013/05/10/best-way-to-transport-your-photo-equipment-including-the-600mm-vr/">Best way to transport your photo equipment including a 600mm/f4 lens</a></li>
-<li>Slidescanner <a href="http://www.nikon.ch/de_CH/product/discontinued/film-scanners/2009/super-coolscan-5000" target="_blank">Nikon Super Coolscan 5000ED</a> (28.05.2004)</li>
+    <li>Gimbal head <a href="https://www.jobu-design.com/product-page/jobu-heavy-duty-mk-iv" target="_blank">Jobu Heavy Duty MK IV</a> (25.04.2016)<br>
+        For more information read my post <a href="/articles/en/2016/04/23/replacement-foot-for-the-nikon-600mm-fl/">Gimbal and replacement foot for the Nikon
+            600mm FL/</a></li>
+    <li>Ballhead <a href="https://www.reallyrightstuff.com/bh55-ball-head" target="_blank">Really Right Stuff BH-55 LR</a> (21.06.2008)</li>
+    <li>Ballhead <a href="https://www.reallyrightstuff.com/bh30-ball-head" target="_blank">Really Right Stuff BH-30-LR</a> (23.06.2012)</li>
+    <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT5541LS</a> (22.07.2008)</li>
+    <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT4552TS</a> (22.10.2012)</li>
+    <li>Tripod <a href="https://www.manfrotto.us/gitzo/tripods/systematic" target="_blank">Gitzo GT3541LS</a> (10.11.2008)</li>
+    <li>Teleconverter Nikon TC-14E III (16.02.2020)<br>
+        For more information read my post <a href="/articles/en/2013/11/23/comparison-of-nikon-300mm-and-600mm-with-teleconverter-tc-14/">Comparison of Nikon
+            300mm and 600mm with Teleconverter TC-14</a></li>
+    <li>GPS <a href="http://di-gps.com/shop/di-GPS-Eco-ProFessional-M-PF1-M" target="_blank">Dawntec di-GPS Eco ProF M</a> (07.2014)</li>
+    <li>Solar panel <a href="http://www.sistech.com/products-9171-0/default.asp" target="_blank">SIStech CHEPRE 18V/18Wp</a> (18.08.2012)<br>
+        For more information read my post <a href="/articles/en/2012/11/25/how-to-recharge-your-camera-batteries-in-the-amazonian-jungle/">How to recharge your
+            camera batteries in the Amazonian jungle</a></li>
+    <li>Camera bag <a href="https://www.tenba.com/" target="_blank">Tenba Vector Topload 2</a> (29.10.2012)</li>
+    <li>Trunk <a href="http://www.peli.com/eu/en/product/watertight-storm-hard-cases/large-case/travel-case/iM2875/" target="_blank">Peli Storm Case iM2875</a>
+        (14.09.2012)<br>
+        For more information read my post <a href="/articles/en/2013/05/10/best-way-to-transport-your-photo-equipment-including-the-600mm-vr/">Best way to
+            transport your photo equipment including a 600mm/f4 lens</a></li>
+    <li>Slidescanner <a href="http://www.nikon.ch/de_CH/product/discontinued/film-scanners/2009/super-coolscan-5000" target="_blank">Nikon Super Coolscan
+            5000ED</a> (28.05.2004)
+    </li>
 </ul>
 <h2>Former equipment</h2>
 <ul class="main">
-<li>Nikon D810 (29.12.2014–28.05.2018)</li>
-<li>Nikon D800 (21.03.2012–03.02.2017)</li>
-<li>Nikon D300 (08.01.2009–18.08.2014)</li>
-<li>Nikon F90x (1996–2004)</li>
-<li>Nikon Coolpix P4 (19.05.2006–06.12.2013)</li>
-<li>Sony Cybershot RX100 (06.12.2013)</li>
-<li>Ricoh Singlex (–2006)</li>
-<li>Nikon 600mm f/4G ED VR AF-S (27.07.2008–30.03.2016)</li>
-<li>Nikon 600mm f/5.6 MF (14.03.1997–27.07.2008)</li>
-<li>Nikon 80-400mm f/4.5-5.6G ED VR AF-S (20.09.2013–20.05.2018)</li>
-<li>Nikon 80-400mm f/4.5-5.6D ED VR AF (27.10.2005–20.09.2013)</li>
-<li>Nikon 18-200mm f/3.5-5.6 G ED VR AF-S (08.01.2009–27.05.2014)</li>
-<li>Teleconverter Nikon TC-14E II (20.10.2008)<br>
-<li>GPS Dawntec di-GPS Pro (17.06.2011)</li>
-<li>GPS Solmeta Geotagger N2 (23.05.2009–2011)</li>
-<li>Gymbal head <a href="https://www.dietmar-nill.de/content/Stativkopf.html">Dietmar Nill</a></li>
+    <li>Nikon D810 (29.12.2014–28.05.2018)</li>
+    <li>Nikon D800 (21.03.2012–03.02.2017)</li>
+    <li>Nikon D300 (08.01.2009–18.08.2014)</li>
+    <li>Nikon F90x (1996–2004)</li>
+    <li>Nikon Coolpix P4 (19.05.2006–06.12.2013)</li>
+    <li>Sony Cybershot RX100 (06.12.2013)</li>
+    <li>Ricoh Singlex (–2006)</li>
+    <li>Nikon 600mm f/4G ED VR AF-S (27.07.2008–30.03.2016)</li>
+    <li>Nikon 600mm f/5.6 MF (14.03.1997–27.07.2008)</li>
+    <li>Nikon 80-400mm f/4.5-5.6G ED VR AF-S (20.09.2013–20.05.2018)</li>
+    <li>Nikon 80-400mm f/4.5-5.6D ED VR AF (27.10.2005–20.09.2013)</li>
+    <li>Nikon 18-200mm f/3.5-5.6 G ED VR AF-S (08.01.2009–27.05.2014)</li>
+    <li>Teleconverter Nikon TC-14E II (20.10.2008)<br>
+    <li>GPS Dawntec di-GPS Pro (17.06.2011)</li>
+    <li>GPS Solmeta Geotagger N2 (23.05.2009–2011)</li>
+    <li>Gymbal head <a href="https://www.dietmar-nill.de/content/Stativkopf.html">Dietmar Nill</a></li>
 </ul>
 <?php echo $bodyEnd->render(); ?>
 </body>
