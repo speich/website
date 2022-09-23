8364f9da916725ec92962d736afe2fa777bab2a8
Simon <simon.speich@gmail.com>
2022-09-23 21:07:22 +0200

    redirect to index page

--- default.php
+++ default.php
@@ -1,4 +1,4 @@
 <?php
 // links here https://www.xatakafoto.com/colecciones/nikon-d800-la-mejor-companera-para-fotografiar-aves-y-mucho-mas
-header('Location: /photo/photodb/photo.php', true, 301);
+header('Location: /index.php', true, 301);
 exit;
