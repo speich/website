12c9d02d251727f7c9f8db74cb2bc33715807734
Simon <simon.speich@gmail.com>
2023-12-09 12:08:34 +0100

    update dependencies

--- photo/photodb/photo.js
+++ photo/photodb/photo.js
@@ -1,10 +1,10 @@
-import PhotoSwipeLightbox from '../../library/photoswipe/5.3.0/photoswipe-lightbox.esm.js';
+import PhotoSwipeLightbox from '../../library/photoswipe/5.4.3/photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
   gallery: '#slides',
 	children: '.slideCanvas a',
   thumbSelector: 'a',
-  pswpModule: () => import('../../library/photoswipe/5.3.0/photoswipe.esm.js')
+  pswpModule: () => import('../../library/photoswipe/5.4.3/photoswipe.esm.js')
 });
 
 lightbox.init();
