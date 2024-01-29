606a432ff5705a19b44570a5aa37391b2a619e5f
Simon <simon.speich@gmail.com>
2024-01-29 19:02:09 +0100

    etag needs to be wrapped in parentheses

--- scripts/php/controller/images/img1.php
+++ scripts/php/controller/images/img1.php
@@ -8,7 +8,7 @@ if (isset($_SERVER['PATH_INFO'], $_GET['w'])) {
     // set headers to allow for caching of photos
     $etag = md5($imgPath.$_GET['w']);   // include width to make different versions of different size, but same image cacheable
     $etagHeader = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false;
-    header("ETag: $etag");
+    header('ETag: "'.$etag.'"');
     header('Cache-Control: max-age=86400');
     if ($etagHeader === $etag) {
         http_response_code(304);    // 304 Not Modified
