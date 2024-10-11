8a5c570fe6aab4d099510c57ff3de15adc0fb5c5
speich <info@speich.net>
2024-10-11 12:29:46 +0200

    update date of last update

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -19,7 +19,7 @@ $language->arrLang = ['de' => 'Deutsch', 'en' => 'English'];
 $language->autoSet();
 
 $web = new WebsiteSpeich();
-$web::setLastUpdate('2024-09-16');
+$web::setLastUpdate('2024-10-11');
 $web->setWebroot('/');
 ini_set('default_charset', $web->charset);
 
