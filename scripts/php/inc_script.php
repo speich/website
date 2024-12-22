05b436cfee16b4b6c894b0d552ae95ba39fd9cda
Simon <info@speich.net>
2024-12-22 16:34:38 +0100

    update date of last update

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -19,7 +19,7 @@ $language->arrLang = ['de' => 'Deutsch', 'en' => 'English'];
 $language->autoSet();
 
 $web = new WebsiteSpeich();
-$web::setLastUpdate('2024-12-19');
+$web::setLastUpdate('2024-12-22');
 $web->setWebroot('/');
 ini_set('default_charset', $web->charset);
 
