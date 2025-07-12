7a25d3fa925b2c0bcbbaf7e4c6f3055b55aaded0
Simon <info@speich.net>
2025-07-12 16:55:16 +0200

    add the latest competition

--- about/auszeichnungen.php
+++ about/auszeichnungen.php
@@ -13,12 +13,32 @@
 <?php echo $bodyStart->render($mainNav, $sideNav, $langNav); ?>
 <h1>Auszeichnungen Fotowettbewerbe</h1>
 <div class="grid-container">
+    <h2>NFS-Wettbewerb 2025</h2>
+    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2025"
+                                     title="NFS-Wettbewerb 2020" target="_blank">NFS-Wettbewerb 2025</a></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=9959"><img src="../photo/photodb/images/thumbs/2023-02-Florida/2023-02-Florida-091.jpg" width="270" height="180"
+                                                                     alt="Foto Schneckenweih im Flug (Rostrhamus sociabilis)" title="Schneckenweih im Flug (Rostrhamus sociabilis)"></a></div>
+    <div>Dosenöffner<br>
+        Kategorie «Vögel», <strong>Lobende Erwähnung</strong></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=12345"><img src="../photo/photodb/images/thumbs/wsl/2024-10-Puschlav/2024-10-Puschlav-016.jpg" width="270" height="180"
+                                                                     alt="Foto Lärchenwald mit Wolkenstimmung" title="Rembrandts Lärchenwald"></a></div>
+    <div>Lärchenwald<br>
+        Kategorie «Landschaften», <strong>Lobende Erwähnung</strong></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=10642"><img src="../photo/photodb/images/thumbs/2022-06-Schweden/2022-06-Schweden-110.jpg" width="270" height="180"
+                                                                      alt="Ein Sterntaucher schwimmt im Wasser beleuchtet von den letzten Sonnenstrahlen." title="einsamer Sterntaucher (Gavia stellata)"></a></div>
+    <div>Nachtruhe<br>
+        Sonderkategorie «Spiegelungen», <strong>Lobende Erwähnung</strong></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=12346"><img src="../photo/photodb/images/thumbs/wsl/2024-10-Puschlav/2024-10-Puschlav-058.jpg" width="270" height="152"
+                                                                      alt="Foto von herbstlichen Lärchen, die sich im Wasser spiegeln." title="verwirrende Spiegelung"></a></div>
+    <div>Weichgezeichnet<br>
+        Sonderkategorie «Spiegelungen», <strong>Lobende Erwähnung</strong></div>
+
     <h2>GDT Naturfotograf des Jahres 2023</h2>
     <div>GDT (Gesellschaft für Naturfotografie) e.V. , <a
             href="https://www.gdtfoto.de/seiten/gdt-naturfotograf-des-jahres-ergebnisse-2023.html"
             target="_blank">GDT Naturfotograf des Jahres 2023</a></div>
     <div><a href="../photo/photodb/photo-detail.php?country=16&imgId=9543"><img src="../photo/photodb/images/2015-09-Australia/2015-09-Australia-576.jpg"
-                width="180" height="120" title="Geisterflieger" alt="ein fliegender Flughund mit durchscheinenden Flügeln"></a></div>
+                width="270" height="180" title="Geisterflieger" alt="ein fliegender Flughund mit durchscheinenden Flügeln"></a></div>
     <div>Geisterflieger<br>
         Kategorie «Säugetiere», <strong>7. Rang</strong></div>
     <h2>Schnappschuss 2022/23</h2>
@@ -26,42 +46,42 @@
                 href="https://www.nmbs.ch/home/info_angebote/mediencorner/pressebilder/Schnappschuss-2022_23.html"
                 target="_blank">Schnappschuss</a></div>
     <div><a href="../articles/de/2023/03/24/2-platz-fur-mein-foto-pfostenschacher/"><img src="../photo/photodb/images/thumbs/2022-05-Bulgarien/2022-05-Bulgarien-126.jpg"
-                    width="180" height="120" alt="High-key Foto von Brandseeschwalben und einer Flussseeschwalbe, die auf Holzpfosten sitzen, die im Wasser stehen."></a></div>
+                    width="270" height="180" alt="High-key Foto von Brandseeschwalben und einer Flussseeschwalbe, die auf Holzpfosten sitzen, die im Wasser stehen."></a></div>
     <div>Pfostenschacher<br>
         Kategorie «Tierporträts/Tiere in ihrem Lebensraum», <strong>2. Rang</strong></div>
     <h2>NFS-Wettbewerb 2020</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2020.html"
+    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2020"
                 title="NFS-Wettbewerb 2020" target="_blank">NFS-Wettbewerb 2020</a></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=8189"><img src="../photo/photodb/images/thumbs/ch/2019-06/2019-07-Schaffhausen-051.jpg" width="180" height="120"
+    <div><a href="../photo/photodb/photo-detail.php?imgId=8189"><img src="../photo/photodb/images/thumbs/ch/2019-06/2019-07-Schaffhausen-051.jpg" width="270" height="180"
                     alt="Foto kleiner Leuchtkäfer (Lamprohiza splendidula)" title="Leuchtspuren im Wald"></a></div>
     <div>Leuchtspuren im Wald<br>
         Kategorie «Andere Tiere», <strong>2. Rang</strong></div>
     <div><a href="../photo/photodb/photo-detail.php?pg=7&country=1&qual=1&q=&imgId=7084"><img src="../photo/photodb/images/thumbs/2017-03-Florida/2017-03-Florida-221.jpg"
-                    width="180" height="120" alt="Foto Grünreiher (Butorides virescens)" title="Stiller Beobachter"></a></div>
+                    width="270" height="180" alt="Foto Grünreiher (Butorides virescens)" title="Stiller Beobachter"></a></div>
     <div>Stiller Beobachter<br>
-        Sonderkategorie «Wasser», <strong>Lobenswerte Erwähnung</strong></div>
+        Sonderkategorie «Wasser», <strong>Lobende Erwähnung</strong></div>
     <h2>NFS-Wettbewerb 2018</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2018.html"
+    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2018"
                 title="NFS-Wettbewerb 2018" target="_blank">NFS-Wettbewerb 2018</a></div>
     <div><a href="../photo/photodb/photo-detail.php?imgId=7673"><img
-                    src="../photo/photodb/images/thumbs/2016-05-Ungarn/2016-05-Ungarn-352.jpg" width="180" height="120" alt="Foto badende Stare"></a></div>
+                    src="../photo/photodb/images/thumbs/2016-05-Ungarn/2016-05-Ungarn-352.jpg" width="270" height="180" alt="Foto badende Stare"></a></div>
     <div>Tinguely<br>
-        Sonderkategorie «Bewegung / Aktion», <strong>Lobenswerte Erwähnung</strong></div>
+        Sonderkategorie «Bewegung / Aktion», <strong>Lobende Erwähnung</strong></div>
     <h2>NFS-Wettbewerb 2017</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2017.html"
+    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2017"
                 title="NFS-Wettbewerb 2017" target="_blank">NFS-Wettbewerb 2017</a></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=7104"><img src="../photo/photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg" width="180"
-                    height="120" alt="Foto einer Haubenmeise"></a></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=7104"><img src="../photo/photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg" width="270"
+                    height="180" alt="Foto einer Haubenmeise"></a></div>
     <div>Haubenmeise<br>
-        Kategorie «Vögel», <strong>Lobenswerte Erwähnung</strong></div>
+        Kategorie «Vögel», <strong>Lobende Erwähnung</strong></div>
     <div><a href="../photo/photodb/photo-detail.php?imgId=3156"><img src="../photo/photodb/images/thumbs/ch/ch012/ch012-031.jpg"
-                    width="180" height="119" alt="Foto von vereisten Bäumen"></a></div>
+                                                                     width="270" height="179" alt="Foto von vereisten Bäumen"></a></div>
     <div>Kitsch<br>
-        Kategorie «Landschaft», <strong>Lobenswerte Erwähnung</strong></div>
+        Kategorie «Landschaft», <strong>Lobende Erwähnung</strong></div>
     <h2>NFS-Wettbewerb 2016</h2>
     <div>Naturfotografen Schweiz, <a href="https://web.archive.org/web/20220128014626/https://www.naturfotografen.ch/wettbewerb/id-2016.html" title="NFS-Wettbewerb 2016" target="_blank">NFS-Wettbewerb
             2016</a></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=3851"><img src="../photo/photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="180" height="119"
+    <div><a href="../photo/photodb/photo-detail.php?imgId=3851"><img src="../photo/photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="270" height="179"
                     alt="Foto von vereisten Bäumen"></a></div>
     <div>Paintbrush<br>
         Kategorie «Nature as Art», <strong>1. Rang</strong></div>
@@ -72,7 +92,7 @@
     <div><a href="../articles/de/2016/04/23/1311/"><img src="../photo/images/helmkasuar.jpg" width="133" height="200" alt="Foto eines Helmkasuars"></a></div>
     <div>Hutmode im Vogelreich<br>
         Kategorie «Black and White», <strong>Highlight</strong></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=4693"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="180" height="120"
+    <div><a href="../photo/photodb/photo-detail.php?imgId=4693"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="270" height="180"
                     alt="Foto von einem Frosch im Spülbecken"></a></div>
     <div>Kategorie «Wildlife in Human Environment», <strong>Endrunde</strong></div>
     <h2>Schnappschuss 2015/16</h2>
@@ -80,41 +100,41 @@
                 href="https://web.archive.org/web/20160317085841/http://www.nmbs.ch/home/info_angebote/mediencorner/pressebilder/schnappschuss-kategorie-2-tiere.html"
                 target="_blank">Schnappschuss</a></div>
     <div><a href="../articles/de/2016/03/11/3-rang-mit-foto-haubemeise/"><img src="../photo/photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg"
-                    width="180" height="120" alt="Foto einer Haubenmeise"></a></div>
+                    width="270" height="180" alt="Foto einer Haubenmeise"></a></div>
     <div>Kategorie «Tierporträts/Lebensraum/Verhalten», <strong>3. Rang</strong></div>
     <h2>GDT europäischer Naturfotograf des Jahres 2015</h2>
     <div></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=4693"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="180"
-                    height="120" alt="Foto Frosch im Spülbecken"></a></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=4693"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="270"
+                    height="180" alt="Foto Frosch im Spülbecken"></a></div>
     <div>Kategorie «Mensch und Natur», <strong>Halbfinal</strong></div>
     <div><a href="../photo/images/2012-10-Morteratsch-054.jpg"><img src="../photo/images/K5_Herbstwald.jpg" width="180" height="120" alt="Herbstlicher Lärchenwald"></a></div>
     <div>Kategorie Landschaften, <strong>Viertelfinal</strong></div>
     <h2>BILDERRAUSCH 2012/13</h2>
     <div>Naturhistorischen Museums Basel, <a href="https://web.archive.org/web/20130403155611/http://www.nmb.bs.ch/wildlife/wildlife_fotowettbewerb.htm"
                 target="_blank">BILDERRAUSCH</a></div>
-    <div><a href="../articles/de/2013/02/21/fotowettbewerb-bilderrausch/"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="180"
-                    height="120" alt="Foto eines Blaufussreihers"></a></div>
+    <div><a href="../articles/de/2013/02/21/fotowettbewerb-bilderrausch/"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="270"
+                    height="180" alt="Foto eines Blaufussreihers"></a></div>
     <div>Kategorie «Tierporträts/Lebensraum/Verhalten», <strong>1. Rang</strong></div>
     <h2>photo.vogelwarte.ch 2012</h2>
-    <div>Schweizerische Vogelwarte Sempach, <a href="http://photo.vogelwarte.ch/gallery-2012/#pic-67" title="Finalist 2012" target="_blank">Fotowettbewerb</a></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=4702"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="180" height="120"
+    <div>Schweizerische Vogelwarte Sempach, <a href="https://photo.vogelwarte.ch/gallery-2012/#pic-67" title="Finalist 2012" target="_blank">Fotowettbewerb</a></div>
+    <div><a href="../photo/photodb/photo-detail.php?imgId=4702"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="270" height="180"
                     alt="Foto eines Fischadlers mit Fisch"></a></div>
     <div><strong>Finalist</strong></div>
     <h2>WWF Magazin</h2>
     <div></div>
-    <div><a href="../photo/photodb/photo-detail.php?imgId=3252"><img src="../photo/photodb/images/thumbs/024/024-029.jpg" width="180" height="119"
+    <div><a href="../photo/photodb/photo-detail.php?imgId=3252"><img src="../photo/photodb/images/thumbs/024/024-029.jpg" width="270" height="179"
                     alt="Foto Eichhörnchen kopfüber"></a></div>
-    <div>Leserfoto <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 2/2012,
+    <div>Leserfoto <a href="https://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 2/2012,
         <strong>2. Rang</strong></div>
     <div><a href="../photo/photodb/photo-detail.php?imgId=3066"><img
-                    src="../photo/photodb/images/thumbs/ch/ch011/ch011-033.jpg" width="119" height="180"
+                    src="../photo/photodb/images/thumbs/ch/ch011/ch011-033.jpg" width="179" height="270"
                     alt="Foto Leute vor Fotoautomat"></a></div>
-    <div>Leserfoto <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 1/2012,
+    <div>Leserfoto <a href="https://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 1/2012,
         <strong>1. Rang</strong></div>
     <h2>basellomorallye.02</h2>
     <div></div>
     <div><a href="../photo/images/016-009.jpg"><img src="../photo/images/016-009-thumb.jpg" width="180" height="117" alt="hot love story"></a></div>
-    <div><a href="http://lomography.ch/2002/09/09/basellomorallye02/" target="_blank">basellomorallye 2002</a>, <strong>1. Rang</strong></div>
+    <div><a href="https://lomography.ch/2002/09/09/basellomorallye02/" target="_blank">basellomorallye 2002</a>, <strong>1. Rang</strong></div>
 </div>
 <?php echo $bodyEnd->render(); ?>
 </body>
