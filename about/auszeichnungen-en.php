02ac8b1793bbdbaaa455ca9fd371a20b02231e93
Simon <info@speich.net>
2025-07-12 18:28:13 +0200

    add the latest competition

--- about/auszeichnungen-en.php
+++ about/auszeichnungen-en.php
@@ -13,54 +13,74 @@
 <?php echo $bodyStart->render($mainNav, $sideNav, $langNav); ?>
 <h1>Photography Awards</h1>
 <div class="grid-container">
+    <h2>NFS-Competition 2025</h2>
+    <div>Swiss Nature Photographers, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2025"
+                                     title="NFS-Competition 2020" target="_blank">NFS-Competition 2025</a></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=9959"><img src="../photo/photodb/images/thumbs/2023-02-Florida/2023-02-Florida-091.jpg" width="270" height="180"
+                                                                     alt="Photo of a snail kite in flight (Rostrhamus sociabilis) with an apple snail in its fangs." title="Snail kite in flight (Rostrhamus sociabilis)"></a></div>
+    <div>The can opener<br>
+        category «Birds», <strong>honorable mention</strong></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=12345"><img src="../photo/photodb/images/thumbs/wsl/2024-10-Puschlav/2024-10-Puschlav-016.jpg" width="270" height="180"
+                                                                      alt="Photo of larch forest with an interesting cloudescape" title="Rembrandt's Larch Forest"></a></div>
+    <div>Larch forest<br>
+        category «Landscapes», <strong>honorable mention</strong></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=10642"><img src="../photo/photodb/images/thumbs/2022-06-Schweden/2022-06-Schweden-110.jpg" width="270" height="180"
+                                                                      alt="A red-throated loon floats on the water and is nicely illuminated by the last sun rays." title="lonely Red-throated Loon (Gavia stellata)"></a></div>
+    <div>Peaceful evening<br>
+        special category «Reflections», <strong>honorable mention</strong></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=12346"><img src="../photo/photodb/images/thumbs/wsl/2024-10-Puschlav/2024-10-Puschlav-058.jpg" width="270" height="152"
+                                                                      alt="Photo of autumnal larches reflected in the water." title="puzzling reflection"></a></div>
+    <div>Soft focus<br>
+        special category «Reflections», <strong>honorable mention</strong></div>
+
     <h2>GDT Nature Photographer of the Year</h2>
     <div>GDT (German society for nature photography), <a
                 href="https://www.gdtfoto.de/seiten/gdt-naturfotograf-des-jahres-results-2023.html?lang=English"
                 target="_blank">GDT Nature Photographer of the Year</a></div>
     <div><a href="../photo/photodb/photo-detail-en.php?country=16&imgId=9543&lang=en"><img src="../photo/photodb/images/2015-09-Australia/2015-09-Australia-576.jpg"
-                    width="180" height="120" title="Ghost in the sky" alt="a flying fox with translucent wings"></a></div>
+                    width="270" height="180" title="Ghost in the sky" alt="a flying fox with translucent wings"></a></div>
     <div>Ghost in the sky<br>
         category «Mammals», <strong>7. place</strong></div>
     <h2>Schnappschuss 2022/23</h2>
     <div>Naturhistorisches Museum Basel, <a
                 href="https://www.nmbs.ch/home/info_angebote/mediencorner/pressebilder/Schnappschuss-2022_23.html" target="_blank">Schnappschuss</a></div>
     <div><a href="../articles/de/2023/03/24/2-platz-fur-mein-foto-pfostenschacher"><img
-                    src="../photo/photodb/images/thumbs/2022-05-Bulgarien/2022-05-Bulgarien-126.jpg" width="180" height="120"
+                    src="../photo/photodb/images/thumbs/2022-05-Bulgarien/2022-05-Bulgarien-126.jpg" width="270" height="180"
                     alt="high-key photo of Sandwich Terns and a Common Tern sitting on a wooden post"></a></div>
     <div>category Animal Portraits/Animals in their Habitat, <strong>2. place</strong></div>
     <h2>NFS-Competition 2020</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2020.html"
-                title="NFS-Wettbewerb 2020" target="_blank">NFS-Wettbewerb 2020</a></div>
-    <div><a href="../photo/photodb/photo-detail-en.php?imgId=8189"><img src="../photo/photodb/images/thumbs/ch/2019-06/2019-07-Schaffhausen-051.jpg" width="180"
-                    height="120"
+    <div>Swiss Nature Photographers, <a href="https://www.naturfotografen.ch/bilder/wettbewerbsbilder/id-2020.html"
+                title="NFS-Competition 2020" target="_blank">NFS-Competition 2020</a></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=8189"><img src="../photo/photodb/images/thumbs/ch/2019-06/2019-07-Schaffhausen-051.jpg" width="270"
+                    height="180"
                     alt="photot of male Central European Lesser Glow-worms (Lamprohiza splendidula)" title="traces of light in the forest"></a></div>
     <div>category "other animals", <strong>2. place</strong></div>
     <div><a href="../photo/photodb/photo-detail-en.php?pg=7&country=1&qual=1&q=&imgId=7084"><img
                     src="../photo/photodb/images/thumbs/2017-03-Florida/2017-03-Florida-221.jpg"
-                    width="180" height="120" alt="photo of a Green Heron (Butorides virescens)" title="silent observer"></a></div>
+                    width="270" height="180" alt="photo of a Green Heron (Butorides virescens)" title="silent observer"></a></div>
     <div>special category "water", <strong>honorable mention</strong></div>
     <h2>NFS-Competition 2018</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2018.html" title="NFS-Wettbewerb 2018" target="_blank">NFS-Wettbewerb
+    <div>Swiss Nature Photographers, <a href="https://www.naturfotografen.ch/wettbewerb/id-2018.html" title="NFS-Competition 2018" target="_blank">NFS-Competition
             2018</a></div>
-    <div><a href="../photo/photodb/photo-detail-en.php?imgId=7673"><img src="../photo/photodb/images/thumbs/2016-05-Ungarn/2016-05-Ungarn-352.jpg" width="180"
-                    height="120"
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=7673"><img src="../photo/photodb/images/thumbs/2016-05-Ungarn/2016-05-Ungarn-352.jpg" width="270"
+                    height="180"
                     alt="photo of starlings taking a bath"></a></div>
     <div>special category "movement / action", <strong>honorable mention</strong></div>
     <h2>NFS-Competition 2017</h2>
-    <div>Naturfotografen Schweiz, <a href="https://www.naturfotografen.ch/wettbewerb/id-2017.html"
-                title="NFS-Wettbewerb 2017" target="_blank">NFS-Wettbewerb 2017</a></div>
+    <div>Swiss Nature Photographers, <a href="https://www.naturfotografen.ch/wettbewerb/id-2017.html"
+                title="NFS-Competition 2017" target="_blank">NFS-Competition 2017</a></div>
     <div><a href="../photo/photodb/photo-detail-en.php?imgId=7104"><img
-                    src="../photo/photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg" width="180" height="120" alt="photo of a crested tit"></a>
+                    src="../photo/photodb/images/thumbs/ch/2015-03-Pontresina/2015-03-Pontresina-005.jpg" width="270" height="180" alt="photo of a crested tit"></a>
     </div>
     <div>category birds, <strong>honorable mention</strong></div>
     <div><a href="../photo/photodb/photo-detail-en.php?imgId=3156"><img src="../photo/photodb/images/thumbs/ch/ch012/ch012-031.jpg"
-                    width="180" height="119" alt="photo of iced over trees"></a></div>
+                                                                        width="270" height="179" alt="photo of iced over trees"></a></div>
     <div>category landscape, <strong>honorable mention</strong></div>
     <h2>NFS-Competition 2016</h2>
-    <div>Naturfotografen Schweiz, <a href="https://web.archive.org/web/20220128014626/https://www.naturfotografen.ch/wettbewerb/id-2016.html"
-                title="NFS-Wettbewerb 2016"
-                target="_blank">NFS-Wettbewerb 2016</a></div>
-    <div><a href="../photo/photodb/photo-detail-en.php?imgId=3851"><img src="../photo/photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="180" height="119"
+    <div>Swiss Nature Photographers, <a href="https://web.archive.org/web/20220128014626/https://www.naturfotografen.ch/wettbewerb/id-2016.html"
+                title="NFS-Competition 2016"
+                target="_blank">NFS-Competition 2016</a></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=3851"><img src="../photo/photodb/images/thumbs/ch/ch014/ch014-078.jpg" width="270" height="179"
                     alt="photo of frosted trees"></a></div>
     <div>category Nature as Art, <strong>1. place</strong></div>
     <h2>Glanzlichter 2016</h2>
@@ -70,7 +90,7 @@
     <div><a href="../articles/de/2016/04/23/1311/"><img src="../photo/images/helmkasuar.jpg" width="133" height="200" alt="photo of a cassowary"></a></div>
     <div>category Black and White, <strong>highlight</strong></div>
     <div><a href="../photo/photodb/photo-detail-en.php?imgId=4693"><img
-                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="180" height="120" alt="photo of a frog in a sink"></a>
+                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" width="270" height="180" alt="photo of a frog in a sink"></a>
     </div>
     <div>category Wildlife in Human Environment, <strong>final round</strong></div>
     <h2>Schnappschuss 2015/16</h2>
@@ -83,33 +103,33 @@
     <h2>GDT European Wildlife Photographer of the Year 2015</h2>
     <div></div>
     <div><a href="../photo/photodb/photo-detail-en.php?imgId=4693"><img
-                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" alt="photo of a frog in a sink" width="180" height="120"></a>
+                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-337.jpg" alt="photo of a frog in a sink" width="270" height="180"></a>
     </div>
     <div>category Man and Nature, <strong>semifinal</strong></div>
     <div><a href="../photo/images/2012-10-Morteratsch-054.jpg"><img src="../photo/images/K5_Herbstwald.jpg" width="180" height="120"
-                    alt="photo of an autumnal larch forest"></a></div>
+                                                                    alt="photo of an autumnal larch forest"></a></div>
     <div>category Landscapes, <strong>quarterfinal</strong></div>
     <h2>BILDERRAUSCH 2012/13</h2>
     <div>Naturhistorischen Museums Basel, <a href="https://web.archive.org/web/20130403155611/http://www.nmb.bs.ch/wildlife/wildlife_fotowettbewerb.htm"
                 target="_blank">BILDERRAUSCH</a></div>
     <div><a href="../articles/de/2013/02/21/fotowettbewerb-bilderrausch/"><img
-                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="180" height="120"
+                    src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-268.jpg" width="270" height="180"
                     alt="photo of a Reddish Egret (Egretta rufescens)"></a></div>
     <div>category Animal Portraits/Habitat/Behavior, <strong>1. place</strong></div>
     <h2>photo.vogelwarte.ch 2012</h2>
     <div>Swiss Ornithological Institute in Sempach, <a href="http://photo.vogelwarte.ch/en/gallery-2012/#pic-67" title="Finalist 2012" target="_blank">photo
             contest</a></div>
-    <div><a href="../photo/photodb/photo-detail-en.php?imgId=4702"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="180"
-                    height="120"
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=4702"><img src="../photo/photodb/images/thumbs/2012-04-Florida/2012-04-Florida-348.jpg" width="270"
+                    height="180"
                     alt="photo of a Osprey with a fish in its claws"></a></div>
     <div><strong>finalist</strong></div>
     <h2>WWF Magazin</h2>
     <div></div>
     <div><a href="../photo/photodb/photo-detail-en.php?imgId=3252"><img
-                    src="../photo/photodb/images/thumbs/024/024-029.jpg" width="180" height="119" alt="photo of a squirrel"></a></div>
+                    src="../photo/photodb/images/thumbs/024/024-029.jpg" width="270" height="179" alt="photo of a squirrel"></a></div>
     <div>reader photos <a href="https://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a> 2/2012, <strong>2. place</strong></div>
-    <div><a href="../photo/photodb/photo-detail-en.php?imgId=3066"><img src="../photo/photodb/images/thumbs/ch/ch011/ch011-033.jpg" width="119" height="180"
-                    alt="photo people at the photo booth"></a></div>
+    <div><a href="../photo/photodb/photo-detail-en.php?imgId=3066"><img src="../photo/photodb/images/thumbs/ch/ch011/ch011-033.jpg" width="179" height="270"
+                                                                        alt="photo people at the photo booth"></a></div>
     <div>reader photos <a href="http://www.wwf.ch/de/aktuell/publikationen/magazin/" target="_blank">WWF Magazin</a>
         1/2012, <strong>1. place</strong></div>
     <h2>basellomorallye.02</h2>
