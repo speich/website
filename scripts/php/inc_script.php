86099d3320568803e27e90f705836cb55f5c6e79
Simon <info@speich.net>
2025-08-31 19:06:47 +0200

    update date of last udpate

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -19,7 +19,7 @@ $language->arrLang = ['de' => 'Deutsch', 'en' => 'English'];
 $language->autoSet();
 
 $web = new WebsiteSpeich();
-$web::setLastUpdate('2025-07-12');
+$web::setLastUpdate('2025-08-31');
 $web->setWebroot('/');
 ini_set('default_charset', $web->charset);
 
